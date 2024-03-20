3bb5426f34eb717ce0be228c3518abaee05327ae
Mike Natanzon <michael@abundance.id>
2024-03-20 03:28:41 -0400

    Temp code to resolve Netlify build issue

---
+++ pages/~/propose.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+
+export default function ProposePage() {
+  useEffect(() => {
+    console.log(`Temp page`);
+  }, []);
+
+  return (
+    <div className='flex-col' style={{width: 'auto', position: 'relative'}}>&nbsp;
+    </div>
+  );
+}
