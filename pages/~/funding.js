fd874d5a4cb1fae2c3dc6e7374dbf1ce845efd68
Mike Natanzon <michael@abundance.id>
2024-03-20 03:33:21 -0400

    Minor temp code fix

--- pages/~/funding.js
+++ pages/~/funding.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export default function FundingPage() {
   useEffect(() => {
     console.log(`Temp page`);
-  }, [project]);
+  }, []);
 
   return (
     <div className='flex-col' style={{width: 'auto', position: 'relative'}}>&nbsp;
