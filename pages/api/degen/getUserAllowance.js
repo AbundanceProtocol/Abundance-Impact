41d673b753cf63c0af3ab269321a7a5cb60e77d5
Mike Natanzon <michael@abundance.id>
2024-04-18 18:15:10 -0400

    Updated $degen remaining allowance api

--- pages/api/degen/getUserAllowance.js
+++ pages/api/degen/getUserAllowance.js
@@ -3,22 +3,34 @@ export default async function handler(req, res) {
   if (req.method === 'GET' && fid) {
     try {
       console.log(fid)
-      const base = "https://degen.tips/";
-      const url = `${base}api/airdrop2/tip-allowance?fid=${fid}`;
-      const response = await fetch(url
-        , {
+      // const base = "https://degen.tips/";
+      // const url = `${base}api/airdrop2/tip-allowance?fid=${fid}`;
+      // const response = await fetch(url, {
+      //   headers: {
+      //     accept: "application/json",
+      //   },
+      // });
+
+      const remainingBase = "https://www.degentip.me/";
+      const remainingUrl = `${remainingBase}api/get_allowance?fid=${fid}`;
+      const remainingBalance = await fetch(remainingUrl, {
         headers: {
           accept: "application/json",
         },
-      }
-      );
+      });
+      const getRemaining = await remainingBalance.json()
       let remaining
       let total
-      if (response) {
-        const userAllowance = await response.json();
-        remaining = userAllowance[0].remaining_allowance
-        total = userAllowance[0].tip_allowance
-        console.log(total, remaining)
+      // if (response) {
+      //   const userAllowance = await response.json();
+      //   remaining = userAllowance[0].remaining_allowance
+      //   total = userAllowance[0].tip_allowance
+      //   console.log(total, remaining)
+      // }
+      if (getRemaining) {
+        remaining = getRemaining.allowance.remaining_allowance
+        total = getRemaining.allowance.tip_allowance
+        console.log(remaining)
       }
       res.status(200).json({ total, remaining });
     } catch (error) {
@@ -29,3 +41,6 @@ export default async function handler(req, res) {
     res.status(405).json({ error: 'Method Not Allowed' });
   }
 }
+
+
+9326
