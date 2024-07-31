c2faf0a27c0bf80319693ab941f26149cd2b7a5e
Mike Natanzon <michael@abundance.id>
2024-07-31 02:25:59 -0400

    Updating Cast Actions for user curation

--- components/Layout/CastActionNav/CastActions/index.js
+++ components/Layout/CastActionNav/CastActions/index.js
@@ -5,19 +5,19 @@ import { IoInformationCircleOutline as Info } from "react-icons/io5";
 import useMatchBreakpoints from '../../../../hooks/useMatchBreakpoints';
 
 const CastActions = () => {
-  const { points } = useContext(AccountContext);
+  const { points, ecoData } = useContext(AccountContext);
   const { isMobile } = useMatchBreakpoints();
 
   return (
     <div className='flex-col' style={{gap: '0.5rem', margin: '8px'}}>
-      <a className="" title={`+1 ${points}`} href={`https://warpcast.com/~/add-cast-action?name=%2B1+%24${points?.substring(1)}&icon=star&actionType=post&postUrl=https%3A%2Fimpact.abundance.id%2Fapi%2Faction%2Fimpact1%3Fpoints=${points?.substring(1)}&description=Curate+Casts+with+the+Impact+App`} target="_blank" rel="noopener noreferrer">
+      <a className="" title={`+1 ${points}`} href={`https://warpcast.com/~/add-cast-action?name=%2B1+%24${points?.substring(1)}&icon=star&actionType=post&postUrl=https%3A%2Fimpact.abundance.id%2Fapi%2Faction%2F${ecoData.curate_user ? 'user' : 'impact'}1%3Fpoints=${points?.substring(1)}&description=Curate+Casts+with+the+Impact+App`} target="_blank" rel="noopener noreferrer">
         <div className='flex-row cast-act' style={{borderRadius: '8px', padding: '8px 4px', alignItems: 'center', justifyContent: 'center', gap: '0.25rem'}}>
           <FaRegStar size={20} />
           <p className={isMobile ? '' : 'left-nav'} style={{padding: '0px', fontSize: '15px', fontWeight: '500'}}>+1 {points}</p>
         </div>
       </a>
 
-      <a className="" title={`+5 ${points}`} href={`https://warpcast.com/~/add-cast-action?name=%2B5+%24${points?.substring(1)}&icon=star&actionType=post&postUrl=https%3A%2Fimpact.abundance.id%2Fapi%2Faction%2Fimpact5%3Fpoints=${points?.substring(1)}&description=Curate+Casts+with+the+Impact+App`} target="_blank" rel="noopener noreferrer">
+      <a className="" title={`+5 ${points}`} href={`https://warpcast.com/~/add-cast-action?name=%2B5+%24${points?.substring(1)}&icon=star&actionType=post&postUrl=https%3A%2Fimpact.abundance.id%2Fapi%2Faction%2F${ecoData.curate_user ? 'user' : 'impact'}5%3Fpoints=${points?.substring(1)}&description=Curate+Casts+with+the+Impact+App`} target="_blank" rel="noopener noreferrer">
         <div className='flex-row cast-act' style={{borderRadius: '8px', padding: '8px 4px', alignItems: 'center', justifyContent: 'center', gap: '0.25rem'}}>
           <FaRegStar size={20} />
           <p className={isMobile ? '' : 'left-nav'} style={{padding: '0px', fontSize: '15px', fontWeight: '500'}}>+5 {points}</p>
