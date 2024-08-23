460305dbc9504feb44e15effe29d814e70a1ab92
Mike Natanzon <michael@abundance.id>
2024-08-23 03:22:22 -0400

    Frame testing

---
+++ pages/api/frames/console/circle.jsx
@@ -0,0 +1,114 @@
+import satori from "satori";
+import path from 'path'
+import fs from 'fs';
+import { promisify } from 'util';
+import svg2img from 'svg2img';
+import fetch from 'isomorphic-unfetch';
+import NodeCache from 'node-cache';
+import Tip from "../../../../models/Tip";
+import Cast from "../../../../models/Cast";
+import Circle from "../../../../models/Circle";
+import connectToDatabase from "../../../../libs/mongodb";
+import { numToText } from "../../../../utils/utils";
+import mongoose from 'mongoose';
+
+const baseURL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL_PROD : process.env.NEXT_PUBLIC_BASE_URL_DEV;
+const cache = new NodeCache({ stdTTL: 60 });
+
+
+export default async function handler(req, res) {
+  const { id } = req.query
+
+  console.log('id', id)
+  try {
+    const fontPath = path.join(process.cwd(), 'public', 'Inter-SemiBold.ttf');
+    const fontData = fs.readFileSync(fontPath);
+
+    async function getCircle(id) {
+      try {
+        const objectId = new mongoose.Types.ObjectId(id)
+        console.log(id)
+        await connectToDatabase();
+        let circle = await Circle.findOne({ _id: objectId }).exec();
+        if (circle) {
+          return {circles: circle.circles, text: circle.text, username: circle.username}
+        } else {
+          return {circles: [], text: '', username: ''}
+        }
+      } catch (error) {
+        console.error("Error while fetching casts:", error);
+        return {circles: [], text: '', username: ''}
+      }  
+    }
+
+    const {circles, text, username} = await getCircle(id);
+
+    const splitCircles = (arr) => {
+      // Calculate the midpoint
+      const midpoint = Math.ceil(arr.length / 2);
+    
+      // Split the array into two parts
+      const firstHalf = arr.slice(0, midpoint);
+      const secondHalf = arr.slice(midpoint);
+    
+      return [firstHalf, secondHalf];
+    };
+
+    const [firstHalf, secondHalf] = splitCircles(circles);
+
+
+    const backgroundImg = `https://impact.abundance.id/images/background.jpg`
+
+    const svg = await satori(
+      <div style={{
+        width: '100%',
+        height: '100%',
+        padding: 30,
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundImage: `url(${backgroundImg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundColor: '#43238a',
+        justifyContent: 'center',
+        alignItems: 'center', 
+      }}>
+        <div style={{gap: '0.5rem', display: 'flex', flexDirection: 'row'}}>
+          {(firstHalf?.length > 0) ? (firstHalf.map((creator, index) => <img key={index} src={creator} width={90} height={90} style={{borderRadius: '80px', border: '2px solid #eee', backgroundColor: '#8363ca'}} />)) : (<div style={{height: '90px'}}>&nbsp;</div>)}
+        </div>
+        <div style={{display: 'flex', flexDirection: 'column', color: 'white', 
+        fontSize: '22px', alignItems: 'center', justifyContent: 'center'}}>
+          <div style={{display: 'flex', textAlign: 'center', color: '#eff', fontSize: '30px', margin: '75px 20px 5px 20px'}}>{`@${username} contributed`}</div>
+          <div style={{display: 'flex', textAlign: 'center', color: '#eff', fontSize: '28px', margin: '5px 20px 5px 20px'}}>{`to ${numToText(circles?.length)} artists and builders`}</div>
+          <div style={{display: 'flex', textAlign: 'center', color: '#cde', fontSize: '24px', margin: '5px 20px 5px 20px'}}>{text}</div>
+          <div style={{display: 'flex', textAlign: 'center', color: '#eff', fontSize: '26px', margin: '5px 20px 75px 20px'}}>{`via /impact`}</div>
+        </div>
+        <div style={{gap: '0.5rem', display: 'flex', flexDirection: 'row'}}>
+          {(secondHalf?.length > 0) ? (secondHalf.map((creator, index) => <img key={index} src={creator} width={90} height={90} style={{borderRadius: '50px', border: '2px solid #eee'}} />)) : (<div style={{height: '90px'}}>&nbsp;</div>)}
+        </div>
+      </div>
+      ,
+      {
+        width: 600, height: 600, 
+        fonts: [{
+          data: fontData, 
+          name: 'Inter', 
+          style: 'normal', 
+          weight: 600
+        }]
+      }
+    );
+
+    const svgBuffer = Buffer.from(svg);
+    const convertSvgToPng = promisify(svg2img);
+    const pngBuffer = await convertSvgToPng(svgBuffer, { format: 'png', width: 600, height: 600 });
+
+    // Set the content type to PNG and send the response
+    res.setHeader('Content-Type', 'image/png');
+    res.setHeader('Cache-Control', 'max-age=10');
+    res.send(pngBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error generating image');
+  }
+}
---
+++ pages/api/frames/console/more.js
@@ -0,0 +1,116 @@
+import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
+import qs from "querystring";
+
+const baseURL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL_PROD : process.env.NEXT_PUBLIC_BASE_URL_DEV;
+const HubURL = process.env.NEYNAR_HUB
+const client = HubURL ? getSSLHubRpcClient(HubURL) : undefined;
+
+
+export default async function handler(req, res) {
+
+  const { untrustedData } = req.body
+  const { iB, qB, qT, author, iA, qA, ec, login, pt, cu, impact, ql, cI, hash, handle } = req.query;
+  
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  } else {
+    // console.log('17', iB, qB, qT, author, iA, qA, ecosystem, login, pt, cu, impact, quality, cI)
+    // console.log('18', typeof iB, typeof cI, typeof impact, typeof quality)
+    const curatorFid = req.body.untrustedData.fid
+
+    let balanceImg = `${baseURL}/api/frames/console/balance?${qs.stringify({ iB, qB, qT, author, iA, qA, ecosystem: ec, login, pt, cu })}`
+
+    let button1 = ''
+    let button2 = ''
+    let button3 = ''
+    let button4 = ''
+    let textField = ''
+    let postUrl = `<meta name="fc:frame:post_url" content='https://impact.abundance.id' />`
+
+    // shareUrl = `https://impact.abundance.id/~/ecosystems/${ecosystem}/tip-share?${qs.stringify({    
+    //   id: circleId })}`
+
+    const shareText = 'I just nominated great builders and creators on /impact. Help support them here:'
+
+    let shareUrl = `https://impact.abundance.id/~/ecosystems/${handle}/tip?${qs.stringify({ time: 'all', shuffle: true, curators: curatorFid, eco: pt?.substring(1), referrer: curatorFid })}`
+    
+
+
+    const encodedShareText = encodeURIComponent(shareText); 
+    let encodedShareUrl = encodeURIComponent(shareUrl); 
+    let shareLink = `https://warpcast.com/~/compose?text=${encodedShareText}&embeds[]=${[encodedShareUrl]}`
+
+
+
+
+    button1 = `<meta property="fc:frame:button:1" content='Multi-tip' />
+    <meta property="fc:frame:button:1:action" content="post" />
+    <meta property="fc:frame:button:1:target" content='https://impact.abundance.id/api/frames/console/tip?${qs.stringify({ time: 'all', curators: curatorFid, eco: pt?.substring(1), ecosystem: handle })}' />`
+    button2 = `<meta property="fc:frame:button:2" content='Share Multi-tip' />
+    <meta property="fc:frame:button:2:action" content="link" />
+    <meta property="fc:frame:button:2:target" content='${shareLink}' />`
+    button3 = `<meta property="fc:frame:button:3" content='< Back' />
+    // <meta property="fc:frame:button:3:action" content="post" />
+    // <meta property="fc:frame:button:3:target" content='https://impact.abundance.id/api/frames/console/status?${qs.stringify({ iB, qB, qT, author, iA, qA, ecosystem: ec, login, pt, cu, impact, quality: ql, cI, hash })}' />`
+    textField = `<meta name="fc:frame:input:text" content="Eg.: 1000 $Degen, 500 $FARTHER" />`
+
+    let metatags = button1 + button2 + button3 + button4 + textField + postUrl
+
+    try {
+
+      res.setHeader('Content-Type', 'text/html');
+      res.status(200)
+      .send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Impact Nav</title>
+            <meta name="fc:frame" content="vNext">
+            <meta property="og:title" content="Impact Nav">
+            <meta property='og:image' content='${balanceImg}' />
+            <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+            <meta property="fc:frame:image" content='${balanceImg}' />
+            ${metatags}
+          </head>
+          <body>
+            <div>Tip frame</div>
+          </body>
+        </html>
+      `);
+      return;
+
+    } catch (error) {
+
+      res.setHeader('Content-Type', 'text/html');
+      res.status(500).send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Impact Nav</title>
+            <meta name="fc:frame" content="vNext">
+            <meta property="og:title" content="Impact Nav">
+            <meta property='og:image' content='${balanceImg}' />
+            <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+            <meta property="fc:frame:image" content='${balanceImg}' />
+            ${metatags}
+          </head>
+          <body>
+            <div>Tip frame</div>
+          </body>
+        </html>
+      `);
+      return;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
