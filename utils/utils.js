91a0dcad9a199da44180a8ce79120c601dfb199d
Mike Natanzon <michael@abundance.id>
2024-08-24 01:40:50 -0400

    Tip text update

--- utils/utils.js
+++ utils/utils.js
@@ -499,7 +499,7 @@ export async function processTips(userFeed, userFid, tokenData, ecosystem, curat
           }
         }
         if (cast.text.length > 0) {
-          cast.text = `I'm tipping:\n${cast.text}\n via ${ecosystemName}/impact\n\n/impact lets you easily multi-tip your favorite artists & builders, schedule tips & share curations with frens`
+          cast.text = `I'm tipping:\n ${cast.text} via ${ecosystemName}/impact\n\n/impact lets you earn rewards while supporting your favorite creators & builders on Farcaster`
         }
       }
     }
