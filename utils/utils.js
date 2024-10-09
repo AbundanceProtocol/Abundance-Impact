3c2d52f67b90b2662a46a520870f23e97215044d
Mike Natanzon <michael@abundance.id>
2024-10-08 20:49:36 -0400

    Mini-app live testing

--- utils/utils.js
+++ utils/utils.js
@@ -738,15 +738,9 @@ export function getChain(chain) {
 
 export function confirmUser(fid, fidPass) {
   try {
-    let decryptedPass = decryptPassword(fidPass, userSecret)
-    let decryptedFid = decryptedPass.slice(10);
-    if (fid == decryptedFid) {
-      console.log('pass1')
-      return true
-    }
     const decodedParam = decodeURIComponent(fidPass);
-    decryptedPass = decryptPassword(decodedParam, userSecret)
-    decryptedFid = decryptedPass.slice(10);
+    let decryptedPass = decryptPassword(decodedParam, userSecret)
+    let decryptedFid = decryptedPass.slice(10);
     if (fid == decryptedFid) {
       console.log('pass2')
       return true
