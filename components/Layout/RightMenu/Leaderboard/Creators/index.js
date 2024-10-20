ee988a598aa49d843b65f6807f4b85bc933c83fc
Mike Natanzon <michael@abundance.id>
2024-10-20 03:29:53 -0400

    Minor fix to creator link

--- components/Layout/RightMenu/Leaderboard/Creators/index.js
+++ components/Layout/RightMenu/Leaderboard/Creators/index.js
@@ -11,7 +11,7 @@ const Creators = ({creator, index}) => {
     <div className='flex-row' style={{alignItems: 'center', justifyContent: 'flex-start'}}>
       <div style={{fontSize: '15px', fontWeight: '700', width: '15px', padding: '6px 35px 6px 0', color: '#cde'}}>{index + 1}</div>
       <img loading="lazy" src={creator?.author_pfp} className="" alt={`${creator?.author_name} avatar`} style={{width: '28px', height: '28px', maxWidth: '28px', maxHeight: '28px', borderRadius: '24px', border: '1px solid #000'}} />
-      <div style={{fontSize: '15px', fontWeight: '500', width: '15px', padding: '6px 10px', textWrap: 'nowrap'}}><Link className="fc-lnk" href={`/~/ecosystems/${ecoData?.ecosystem_handle}/creators/${creator?.username}`} onClick={(event) => {if (!store.isAuth) {
+      <div style={{fontSize: '15px', fontWeight: '500', width: '15px', padding: '6px 10px', textWrap: 'nowrap'}}><Link className="fc-lnk" href={`/~/ecosystems/${ecoData?.ecosystem_handle}/creators/${creator?.author_name}`} onClick={(event) => {if (!store.isAuth) {
         console.log('cr1')
         event.preventDefault()
         LoginPopup()
