e7ea004fac0f25e71203fea121c9eef265d624e0
Mike Natanzon <michael@abundance.id>
2024-10-28 22:55:13 -0400

    Added composer action

--- components/Layout/RightMenu/index.js
+++ components/Layout/RightMenu/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Leaderboard from './Leaderboard';
 import Ecoboard from './Ecoboard';
+import { useRouter } from 'next/router';
 
 const RightMenu = () => {
+  const router = useRouter()
 
   return (
-    <div className='right-nav-text' style={{width: '400px'}}>
+    router.route !== '/~/studio/multi-tip-compose' && (<div className='right-nav-text' style={{width: '400px'}}>
       <div>
         <Ecoboard />
         <Leaderboard />
       </div>
-    </div>
+    </div>)
   );
 };
 
---
+++ pages/api/composer-action/multi-tip.js
@@ -0,0 +1,36 @@
+import qs from "querystring";
+import { init, validateFramesMessage } from "@airstack/frames";
+const baseURL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL_PROD : process.env.NEXT_PUBLIC_BASE_URL_DEV;
+const userSecret = process.env.USER_SECRET
+
+export default async function handler(req, res) {
+  init(process.env.AIRSTACK_API_KEY ?? '')
+  const body = await req.body;
+  const {isValid, message} = await validateFramesMessage(body)
+  const curatorFid = message?.data?.fid
+
+  if (req.method === 'POST') {
+
+    res.status(200).json({ 
+      type: 'form',
+      title: 'Curator page',
+      url: `${baseURL}/~/studio/multi-tip-compose?${qs.stringify({ fid: curatorFid })}`,
+    });
+    return
+   
+  } else if (req.method === 'GET') {
+    res.status(200).json({
+      "type": "composer",
+      "name": "Curator page",
+      "icon": "person",
+      "description": "Curator",
+      "aboutUrl": `${baseURL}/~/studio/multi-tip-compose?${qs.stringify({ fid: curatorFid })}`,
+      "imageUrl": `${baseURL}/images/input.jpg`,
+      "action": {
+        "type": "post",
+      }
+    });
+  } else {
+    res.status(405).end(); // Method Not Allowed
+  }
+}
