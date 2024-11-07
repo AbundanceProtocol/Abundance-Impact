fb39ca74049bba374ec18b5ac3015be64d567c5b
Mike Natanzon <michael@abundance.id>
2024-11-06 21:00:43 -0500

    Update search

--- pages/api/curation/getUserSearch.js
+++ pages/api/curation/getUserSearch.js
@@ -120,8 +120,12 @@ export default async function handler(req, res) {
     //   query.cast_tags = { $in: [req.query['tags[]']] };
     // }
 
+    if (req?.query?.channels) {
+      query.channel_id = { $in: req.query.channels }
+    }
+
     if (req?.query?.channel) {
-      query.channel_id = { $in: req.query.channel}
+      query.channel_id = { $in: req.query.channel }
     }
 
     // if (req.query['channel[]'] && req.query['channel[]'].length > 0) {
