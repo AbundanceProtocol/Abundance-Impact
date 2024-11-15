f64e283d023743e24e56a72ebc9132bdc329e453
Mike Natanzon <michael@abundance.id>
2024-11-14 22:23:54 -0500

    Testing cast action

---
+++ pages/api/frames/admin/degen5000.js
@@ -0,0 +1,156 @@
+import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
+import qs from "querystring";
+import OptOut from "../../../../models/OptOut";
+import connectToDatabase from "../../../../libs/mongodb";
+import User from '../../../../models/User';
+import Impact from '../../../../models/Impact';
+import Quality from '../../../../models/Quality';
+import Cast from "../../../../models/Cast";
+import EcosystemRules from "../../../../models/EcosystemRules";
+import { init, validateFramesMessage } from "@airstack/frames";
+import { decryptPassword } from "../../../../utils/utils";
+import Tip from "../../../../models/Tip";
+
+const baseURL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL_PROD : process.env.NEXT_PUBLIC_BASE_URL_DEV;
+const HubURL = process.env.NEYNAR_HUB
+const client = HubURL ? getSSLHubRpcClient(HubURL) : undefined;
+const apiKey = process.env.NEYNAR_API_KEY
+const secretKey = process.env.SECRET_KEY;
+const encryptedTipUuid = process.env.ENCRYPTED_TIP_UUID
+const userFid = process.env.USER_FID
+
+export default async function handler(req, res) {
+  init(process.env.AIRSTACK_API_KEY ?? '')
+  const body = await req.body;
+  const {isValid, message} = await validateFramesMessage(body)
+
+  if (req.method === 'POST') {
+    // const points = req.query.pt
+    // const eco = points?.substring(1)
+    const curatorFid = message?.data?.fid
+    const castHash = req.body.untrustedData.castId.hash
+    const authorFid = message?.data?.frameActionBody?.castId?.fid
+
+
+    if (curatorFid !== 9326 && curatorFid !== userFid) {
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+
+
+    const decryptedUuid = decryptPassword(encryptedTipUuid, secretKey);
+
+    const castText = `I'm tipping:\n25 $DEGEN\nvia Abundance Ecosystem on /impact\n\n/impact lets you earn curator rewards while supporting your favorite creators & builders on Farcaster`
+
+    const tips = { currency: '$degen', amount: 25 }
+
+    async function sendTip(signer) {
+      const base = "https://api.neynar.com/";
+      const url = `${base}v2/farcaster/cast`;
+      const body = {
+        signer_uuid: signer,
+        text: castText,
+        parent: castHash
+      };
+    
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'api_key': apiKey,
+          },
+          body: JSON.stringify(body),
+        });
+    
+        if (!response.ok) {
+          console.error(`Failed to send request for ${body.text}`);
+          return 0;
+        }
+        
+        await Tip.create({
+          receiver_fid: authorFid,
+          tipper_fid: 9326,
+          auto_tip: false,
+          points: '$IMPACT',
+          cast_hash: castHash,
+          tip: tips,
+        });
+    
+        return 1;
+      } catch (error) {
+        console.error(`Error in sendTip for ${cast.text}:`, error);
+        return 0;
+      }
+    }
+
+    const tipped = await sendTip(decryptedUuid)
+
+    let balanceImg = ``
+    if (tipped !== 0) {
+      balanceImg = `${baseURL}/api/frames/admin/frame?${qs.stringify({ confirmed: 10, error: null })}`
+    } else {
+      balanceImg = `${baseURL}/api/frames/admin/frame?${qs.stringify({ confirmed: null, error: 404 })}`
+    }
+
+
+    let button1 = ''
+    let button2 = ''
+    let button3 = ''
+    let button4 = ''
+    let textField = ''
+    let postUrl = `<meta name="fc:frame:post_url" content='https://impact.abundance.id' />`
+
+    let metatags = button1 + button2 + button3 + button4 + textField + postUrl
+
+    try {
+
+      res.setHeader('Content-Type', 'text/html');
+      res.status(200)
+      .send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Impact Nav</title>
+            <meta name="fc:frame" content="vNext">
+            <meta property="og:title" content="Impact Nav">
+            <meta property='og:image' content='${balanceImg}' />
+            <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+            <meta property="fc:frame:image" content='${balanceImg}' />
+            ${metatags}
+          </head>
+          <body>
+            <div>Tip frame</div>
+          </body>
+        </html>
+      `);
+      return;
+
+    } catch (error) {
+
+      res.setHeader('Content-Type', 'text/html');
+      res.status(500).send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Impact Nav</title>
+            <meta name="fc:frame" content="vNext">
+            <meta property="og:title" content="Impact Nav">
+            <meta property='og:image' content='${balanceImg}' />
+            <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+            <meta property="fc:frame:image" content='${balanceImg}' />
+            ${metatags}
+          </head>
+          <body>
+            <div>Tip frame</div>
+          </body>
+        </html>
+      `);
+      return;
+    }
+
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(401).send(`Request failed`);
+  }
+}
---
+++ pages/api/frames/admin/status.js
@@ -0,0 +1,98 @@
+import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
+import qs from "querystring";
+import { init, validateFramesMessage } from "@airstack/frames";
+
+const baseURL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL_PROD : process.env.NEXT_PUBLIC_BASE_URL_DEV;
+const HubURL = process.env.NEYNAR_HUB
+const client = HubURL ? getSSLHubRpcClient(HubURL) : undefined;
+
+
+export default async function handler(req, res) {
+  init(process.env.AIRSTACK_API_KEY ?? '')
+  const body = await req.body;
+  const {isValid, message} = await validateFramesMessage(body)
+  console.log('isValid:', isValid)
+  const { untrustedData } = req.body
+  const { confirmed } = req.query;
+
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  } else {
+    
+    let balanceImg = `${baseURL}/api/frames/admin/frame?${qs.stringify({ confirmed: confirmed || null })}`
+
+    let button1 = ''
+    let button2 = ''
+    let button3 = ''
+    let button4 = ''
+    let textField = ''
+    let postUrl = `<meta name="fc:frame:post_url" content='https://impact.abundance.id' />`
+
+
+    if (confirmed) {
+      console.log('1')
+      button1 = ``
+      button2 = ``
+    } else {
+      console.log('1b')
+      button1 = `<meta property="fc:frame:button:1" content='1000 $degen' />
+      <meta property="fc:frame:button:1:action" content="post" />
+      <meta property="fc:frame:button:1:target" content='https://impact.abundance.id/api/frames/admin/degen1000' />`
+      button2 = `<meta property="fc:frame:button:2" content='5000 $degen' />
+      <meta property="fc:frame:button:2:action" content="post" />
+      <meta property="fc:frame:button:2:target" content='https://impact.abundance.id/api/frames/admin/degen5000' />`
+      button3 = ''
+      button4 = ''
+      textField = ''
+    }
+
+    let metatags = button1 + button2 + button3 + button4 + textField + postUrl
+
+    try {
+
+      res.setHeader('Content-Type', 'text/html');
+      res.status(200)
+      .send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Impact Nav</title>
+            <meta name="fc:frame" content="vNext">
+            <meta property="og:title" content="Impact Nav">
+            <meta property='og:image' content='${balanceImg}' />
+            <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+            <meta property="fc:frame:image" content='${balanceImg}' />
+            ${metatags}
+          </head>
+          <body>
+            <div>Tip frame</div>
+          </body>
+        </html>
+      `);
+      return;
+
+    } catch (error) {
+
+      res.setHeader('Content-Type', 'text/html');
+      res.status(500).send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Impact Nav</title>
+            <meta name="fc:frame" content="vNext">
+            <meta property="og:title" content="Impact Nav">
+            <meta property='og:image' content='${balanceImg}' />
+            <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+            <meta property="fc:frame:image" content='${balanceImg}' />
+            ${metatags}
+          </head>
+          <body>
+            <div>Tip frame</div>
+          </body>
+        </html>
+      `);
+      return;
+    }
+  }
+}
