10544f3ae5fb8fb59a7da62702831ea810c31273
Mike Natanzon <50894218+AbundanceProtocol@users.noreply.github.com>
2024-11-28 12:27:12 -0500

    Added stats dashboard

---
+++ pages/api/curation/getStats.js
@@ -0,0 +1,349 @@
+import { decryptPassword } from '../../../utils/utils';
+import connectToDatabase from '../../../libs/mongodb';
+import ScheduleTip from '../../../models/ScheduleTip';
+import Cast from '../../../models/Cast';
+import Impact from '../../../models/Impact';
+import User from '../../../models/User';
+import Tip from '../../../models/Tip';
+import EcosystemRules from '../../../models/EcosystemRules';
+import { getTimeRange, processTips, populateCast } from '../../../utils/utils';
+
+const secretKey = process.env.SECRET_KEY
+const apiKey = process.env.NEYNAR_API_KEY
+
+export default async function handler(req, res) {
+  const { fid } = req.query
+
+  if (req.method !== 'GET') {
+    res.status(405).json({ error: 'Method Not Allowed', message: 'Failed to provide required data' });
+  } else {
+
+
+    // async function getTopCuratorsByImpactPoints() {
+    //   await connectToDatabase();
+    //   const twentyFourHoursAgo = new Date();
+    //   twentyFourHoursAgo.setHours(twentyFourHoursAgo.getHours() - 24);
+
+    //   const result = await Impact.aggregate([
+    //     { $match: { createdAt: { $gte: twentyFourHoursAgo } } },
+    //     { $group: { _id: "$curator_fid", totalImpactPoints: { $sum: "$impact_points" } } },
+    //     { $sort: { totalImpactPoints: -1 } },
+    //     { $limit: 5 }
+    //   ]);
+
+    //   return result
+    // }
+
+    // async function getTop5UsersByTips() {
+    //   await connectToDatabase();
+
+    //   const result = await Tip.aggregate([
+    //     { $group: { _id: "$tipper_fid", totalTips: { $sum: 1 } } },
+    //     { $sort: { totalTips: -1 } },
+    //     { $limit: 5 }
+    //   ]);
+
+    //   return result;
+    // }
+
+    async function aggregateTipsByCurrency() {
+      await connectToDatabase();
+
+      const result = await Tip.aggregate([
+        { $unwind: "$tip" },
+        {
+          $group: {
+            _id: { $toLower: "$tip.currency" },
+            totalAmount: { $sum: "$tip.amount" }
+          }
+        },
+        { $sort: { totalAmount: -1 } }
+      ]);
+
+      return result;
+    }
+
+    async function countUniqueTippers() {
+      await connectToDatabase();
+
+      const result = await Tip.aggregate([
+        { $group: { _id: "$tipper_fid" } },
+        { $count: "uniqueTipperCount" }
+      ]);
+
+      return result[0]?.uniqueTipperCount || 0;
+    }
+
+    async function countUniqueCurators() {
+      await connectToDatabase();
+
+      const result = await Impact.aggregate([
+        {
+          $group: {
+            _id: "$curator_fid",
+            totalImpactPoints: { $sum: "$impact_points" }
+          }
+        },
+        {
+          $match: {
+            totalImpactPoints: { $gte: 1 } // min points staked
+          }
+        },
+        {
+          $count: "uniqueCuratorCount"
+        }
+      ]);
+
+      return result[0]?.uniqueCuratorCount || 0;
+    }
+
+    async function countActiveCurators() {
+      await connectToDatabase();
+
+      const result = await Impact.aggregate([
+        {
+          $match: {
+            impact_points: { $gte: 1 }
+          }
+        },
+        {
+          $group: {
+            _id: {
+              curator_fid: "$curator_fid",
+              date: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } }
+            }
+          }
+        },
+        {
+          $group: {
+            _id: "$_id.curator_fid",
+            uniqueDays: { $sum: 1 }
+          }
+        },
+        {
+          $match: {
+            uniqueDays: { $gte: 10 }
+          }
+        },
+        {
+          $count: "activeCuratorCount"
+        }
+      ]);
+
+      return result[0]?.activeCuratorCount || 0;
+    }
+
+
+    async function countUniqueUsers() {
+      await connectToDatabase();
+
+      const result = await User.aggregate([
+        {
+          $group: {
+            _id: "$fid"
+          }
+        },
+        {
+          $count: "uniqueUserCount"
+        }
+      ]);
+
+      return result[0]?.uniqueUserCount || 0;
+    }
+
+    async function countUniqueCastAuthors() {
+      await connectToDatabase();
+
+      const result = await Cast.aggregate([
+        {
+          $group: {
+            _id: "$author_fid"
+          }
+        },
+        {
+          $count: "uniqueAuthorCount"
+        }
+      ]);
+
+      return result[0]?.uniqueAuthorCount || 0;
+    }
+
+    async function countUniqueImpactedCasts() {
+      await connectToDatabase();
+
+      const result = await Impact.aggregate([
+        {
+          $match: {
+            impact_points: { $gte: 1 }
+          }
+        },
+        {
+          $group: {
+            _id: "$target_cast_hash"
+          }
+        },
+        {
+          $count: "uniqueImpactedCastCount"
+        }
+      ]);
+
+      return result[0]?.uniqueImpactedCastCount || 0;
+    }
+
+
+    async function getTotalDegenTipsForSetCasts() {
+      await connectToDatabase();
+
+      const result = await User.aggregate([
+        { $match: { set_cast_hash: { $exists: true, $ne: null } } },
+        {
+          $lookup: {
+            from: 'tips',
+            localField: 'set_cast_hash',
+            foreignField: 'cast_hash',
+            as: 'matchingTips'
+          }
+        },
+        { $unwind: '$matchingTips' },
+        { $unwind: '$matchingTips.tip' },
+        {
+          $match: {
+            'matchingTips.tip.currency': { $regex: /^degen$/i }
+          }
+        },
+        {
+          $group: {
+            _id: null,
+            totalAmount: { $sum: '$matchingTips.tip.amount' }
+          }
+        }
+      ]);
+
+      return result[0]?.totalAmount || 0;
+    }
+
+
+
+    
+    async function getTipCountsAndAmountsByCastHash(tipper_fid) {
+      await connectToDatabase();
+
+      const result = await Tip.aggregate([
+        { $match: { tipper_fid: parseInt(tipper_fid) } },
+        { $unwind: "$tip" },
+        {
+          $group: {
+            _id: "$cast_hash",
+            count: { $sum: 1 },
+            totalAmount: { $sum: "$tip.amount" }
+          }
+        },
+        { $sort: { count: -1 } }
+      ]);
+
+      // Count how many documents are in each "count" by number
+      const countByNumber = result.reduce((acc, cur) => {
+        acc[cur.count] = (acc[cur.count] || 0) + 1;
+        return acc;
+      }, {});
+
+      return { result, countByNumber };
+    }
+
+    // async function getTipCountsAndAmountsByCastHash(tipper_fid) {
+    //   await connectToDatabase();
+
+    //   const result = await Tip.aggregate([
+    //     { $match: { tipper_fid: parseInt(tipper_fid) } },
+    //     { $unwind: "$tip" },
+    //     {
+    //       $group: {
+    //         _id: "$cast_hash",
+    //         count: { $sum: 1 },
+    //         totalAmount: { $sum: "$tip.amount" }
+    //       }
+    //     },
+    //     { $sort: { count: -1 } }
+    //   ]);
+
+    //   return result;
+    // }
+
+    // async function getTipCountsAndAmountsByCastHash(tipper_fid) {
+    //   await connectToDatabase();
+
+    //   const result = await Tip.aggregate([
+    //     { $match: { tipper_fid: parseInt(tipper_fid) } },
+    //     { $unwind: "$tip" },
+    //     {
+    //       $group: {
+    //         _id: { cast_hash: "$cast_hash", currency: "$tip.currency" },
+    //         count: { $sum: 1 },
+    //         totalAmount: { $sum: "$tip.amount" }
+    //       }
+    //     },
+    //     { $sort: { count: -1 } }
+    //   ]);
+
+    //   return result;
+    // }
+
+
+
+    // async function getTotalTipsByAllCurrencies() {
+    //   await connectToDatabase();
+
+    //   const result = await Tip.aggregate([
+    //     { $unwind: "$tip" },
+    //     { 
+    //       $group: {
+    //         _id: { $toLower: "$tip.currency" },
+    //         totalAmount: { $sum: "$tip.amount" }
+    //       }
+    //     },
+    //     { $sort: { _id: 1 } }
+    //   ]);
+      
+    //   // Combine results for currencies that differ only in case
+    //   const combinedResult = result.reduce((acc, curr) => {
+    //     const existingCurrency = acc.find(item => item._id.toLowerCase() === curr._id.toLowerCase());
+    //     if (existingCurrency) {
+    //       existingCurrency.totalAmount += curr.totalAmount;
+    //     } else {
+    //       acc.push(curr);
+    //     }
+    //     return acc;
+    //   }, []);
+      
+    //   console.log('Total Amount for all currencies:', combinedResult);
+    //   return combinedResult;
+    // }
+    async function countActiveScheduledTips() {
+      await connectToDatabase();
+      
+      const result = await ScheduleTip.countDocuments({ active_cron: true });
+      
+      return result;
+    }
+    
+    try {
+      const getTips = await aggregateTipsByCurrency(fid)
+      // const getTopTips = await getTop5UsersByTips()
+      const uniqueTips = await countUniqueTippers()
+      const uniqueCurator = await countUniqueCurators()
+      const activeCurators = await countActiveCurators()
+      const uniqueUsers = await countUniqueUsers()
+      const uniqueCreators = await countUniqueCastAuthors()
+      const curatedCasts = await countUniqueImpactedCasts()
+      // const degenCuratorRewards = await getTotalDegenTipsForSetCasts()
+      const autoTips = await countActiveScheduledTips();
+      console.log('tips', getTips, uniqueTips, uniqueCurator, uniqueUsers, uniqueCreators, curatedCasts, autoTips)
+      // const totalAmount = await getTotalTipsByAllCurrencies()
+      // console.log('totalAmount', totalAmount)
+      res.status(200).json({ message: 'tips', getTips, uniqueTips, uniqueCurator, activeCurators, uniqueUsers, uniqueCreators, curatedCasts, autoTips });
+    } catch (error) {
+      console.error('Error handling GET request:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
---
+++ pages/~/stats.js
@@ -0,0 +1,203 @@
+import Head from 'next/head';
+import React, { useContext, useState, useRef, useEffect } from 'react'
+import { AccountContext } from '../../context'
+import Link from 'next/link'
+import useMatchBreakpoints from '../../hooks/useMatchBreakpoints'
+// import useStore from '../../utils/store'
+import axios from 'axios';
+// import { FaPen, FaPlus } from "react-icons/fa"
+import { useRouter } from 'next/router';
+import { formatNum, isAlphanumeric, getToken, getChain } from '../../utils/utils';
+// import Spinner from '../../components/Common/Spinner';
+// import Button from '../../components/Ecosystem/Button';
+// import Description from '../../components/Ecosystem/Description';
+// import InputField from '../../components/Ecosystem/InputField';
+// import Dropdown from '../../components/Ecosystem/Dropdown';
+// import { GrFormPrevious, GrFormNext } from "react-icons/gr";
+import Modal from '../../components/Layout/Modals/Modal';
+// import Checkbox from '../../components/Ecosystem/Checkbox';
+import Spinner from '../../components/Common/Spinner';
+import { BsClock } from "react-icons/bs";
+
+
+export default function Curators() {
+  const ref = useRef(null)
+  const { isMobile } = useMatchBreakpoints();
+  const { points, fid, autotipping, setAutotipping, isLogged, LoginPopup } = useContext(AccountContext)
+  const [screenWidth, setScreenWidth] = useState(undefined)
+  const [screenHeight, setScreenHeight] = useState(undefined)
+  // const store = useStore()
+  const [textMax, setTextMax] = useState('562px')
+  const [feedMax, setFeedMax ] = useState('620px')
+  const [curators, setCurators] = useState(null)
+  const router = useRouter()
+  const [modal, setModal] = useState({on: false, success: false, text: ''})
+  // const [jobScheduled, setJobScheduled] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  // const [ecoPoints, setEcoPoints] = useState(null)
+  // const [timeframe, setTimeframe] = useState('24h')
+  const [page, setPage] = useState(1)
+  const { ecosystem } = router.query;
+  const initStats = {activeCurators: 0, uniqueCurators: 0, users: 0, degen: 0, hunt: 0, ham: 0, casts: 0, creators: 0, autoTip: 0}
+  const [stat, setStat] = useState(initStats)
+  const [sched, setSched] = useState({stats: false, setPoints: false})
+
+  
+  useEffect(() => {
+    if (sched.stats) {
+      getSystemStats()
+      setSched(prev => ({...prev, stats: false }))
+    } else {
+      const timeoutId = setTimeout(() => {
+        getSystemStats()
+        setSched(prev => ({...prev, stats: false }))
+      }, 300);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [router.query, fid, sched.stats]);
+
+
+  useEffect(() => {
+    if (screenWidth) {
+      if (screenWidth > 680) {
+        setTextMax(`562px`)
+        setFeedMax('620px')
+      }
+      else if (screenWidth >= 635 && screenWidth <= 680) {
+        setTextMax(`${screenWidth - 120}px`)
+        setFeedMax('580px')
+      }
+      else {
+        setTextMax(`${screenWidth - 10}px`)
+        setFeedMax(`${screenWidth}px`)
+      }
+    }
+    else {
+      setTextMax(`100%`)
+      setFeedMax(`100%`)
+    }
+  }, [screenWidth])
+
+
+  useEffect(() => {
+    console.log('e2 triggered')
+
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth)
+      setScreenHeight(window.innerHeight)
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+
+  async function getSystemStats() {
+    console.log('trigger getSystemStats')
+    try {
+      const response = await axios.get('/api/curation/getStats'
+        // , { params: { fid } }
+      )
+      console.log('response', response?.data)
+      if (response?.data) {
+        const statData = response?.data
+        // console.log('userAutotips', userAutotips)
+        const degen = statData?.getTips?.find(tip => tip._id === '$degen')?.totalAmount || 0;
+        const ham = statData?.getTips?.find((tip) => tip._id === "$tn100x")?.totalAmount || 0;
+        const hunt =
+          statData?.getTips?.find((tip) => tip._id === "$hunt")?.totalAmount ||
+          0;
+        setStat({
+          activeCurators: statData?.activeCurators || 0,
+          uniqueCurators: statData?.uniqueCurator || 0,
+          users: statData?.uniqueUsers || 0,
+          degen: formatNum(degen),
+          ham: formatNum(ham),
+          hunt: formatNum(hunt),
+          casts: statData?.curatedCasts || 0,
+          creators: statData?.uniqueCreators || 0,
+          autoTip: statData?.autoTips || 0,
+        });
+      } else {
+        setStat(initStats)
+      }
+    } catch (error) {
+      console.error('Error submitting data:', error)
+      setAutotipping([])
+    }
+  }
+
+
+  return (
+  <div name='feed' style={{width: 'auto', maxWidth: '620px'}} ref={ref}>
+    <Head>
+      <title>Impact App | Abundance Protocol</title>
+      <meta name="description" content={`Building the global superalignment layer`} />
+    </Head>
+    <div style={{padding: '58px 0 0 0', width: feedMax}}>
+    </div>
+
+    <div title='Cast Actions' className='flex-row' style={{alignItems: 'center', justifyContent: 'center', margin: '8px'}}>
+      <p className='' style={{padding: '10px', color: '#fff', fontWeight: '700', fontSize: '20px'}}>/impact Stat Dashboard </p>
+    </div>
+    
+
+    <div className='flex-row' style={{padding: '10px 0 0 0', flexWrap: 'wrap', minWidth: feedMax, gap: '0.5rem', justifyContent: 'center'}}>
+      {stat.casts > 0 ? (
+        <div className='flex-col' style={{gap: '0.5rem'}}>
+          <div className='curator-frame' style={{gap: '1.5rem', minWidth: isMobile ? '200px' : '250px'}}>
+            <div style={{fontSize: isMobile ? '16px' : '17px', fontWeight: '400', color: '#eff'}}>Total users: {stat?.users}</div>
+          </div>
+
+          <div className='curator-frame' style={{gap: '1.5rem', minWidth: isMobile ? '200px' : '250px'}}>
+            <div style={{fontSize: isMobile ? '16px' : '17px', fontWeight: '400', color: '#eff'}}>Total curators: {stat?.uniqueCurators}</div>
+          </div>
+
+          <div className='curator-frame' style={{gap: '1.5rem', minWidth: isMobile ? '200px' : '250px'}}>
+            <div style={{fontSize: isMobile ? '16px' : '17px', fontWeight: '400', color: '#eff'}}>Active curators: {stat?.activeCurators}</div>
+          </div>
+
+          <div className='curator-frame' style={{gap: '1.5rem', minWidth: isMobile ? '200px' : '250px'}}>
+            <div style={{fontSize: isMobile ? '16px' : '17px', fontWeight: '400', color: '#eff'}}>Casts curated: {stat?.casts}</div>
+          </div>
+
+          <div className='curator-frame' style={{gap: '1.5rem', minWidth: isMobile ? '200px' : '250px'}}>
+            <div style={{fontSize: isMobile ? '16px' : '17px', fontWeight: '400', color: '#eff'}}>Creators curated: {stat?.creators}</div>
+          </div>
+
+          <div className='curator-frame' style={{gap: '1.5rem', minWidth: isMobile ? '200px' : '250px'}}>
+            <div style={{fontSize: isMobile ? '16px' : '17px', fontWeight: '400', color: '#eff'}}>Users auto-tipping: {stat?.autoTip}</div>
+          </div>
+
+          <div className='curator-frame' style={{gap: '1.5rem', minWidth: isMobile ? '200px' : '250px'}}>
+            <div style={{fontSize: isMobile ? '16px' : '17px', fontWeight: '400', color: '#eff', paddingBottom: '10px'}}>Total tipped thru /impact:</div>
+            <div style={{fontSize: isMobile ? '16px' : '17px', fontWeight: '400', color: '#eff'}}>$degen: {stat?.degen}</div>
+            <div style={{fontSize: isMobile ? '16px' : '17px', fontWeight: '400', color: '#eff'}}>$ham: {stat?.ham}</div>
+            <div style={{fontSize: isMobile ? '16px' : '17px', fontWeight: '400', color: '#eff'}}>$hunt: {stat?.hunt}</div>
+          </div>
+        </div>
+      ) : (
+        <>
+          {!loaded ? (<div className='flex-row' style={{height: '100%', alignItems: 'center', width: '100%', justifyContent: 'center', padding: '20px'}}>
+            <Spinner size={31} color={'#999'} />
+          </div>) : (<div style={{fontSize: '20px', color: '#def'}}>No stats found</div>)}
+        </>
+      )}
+
+    </div>
+
+    {(loaded && curators?.length > 0 && curators?.length % 10 == 0) && (<div className='flex-row' style={{height: '30px', alignItems: 'center', width: '100%', justifyContent: 'center', padding: '20px', margin: '10px 0 50px 0'}}>
+      <div className='flex-row' style={{padding: '4px 8px', backgroundColor: '#33445522', border: '1px solid #666', borderRadius: '20px', alignItems: 'center', gap: '0.25rem'}}>
+        <div className={'filter-item'} onClick={() => {updatePage(page)}} style={{fontSize: '12px'}}>Load more</div>
+      </div>
+    </div>)}
+
+    <div style={{margin: '0 0 70px 0'}}></div>
+    <Modal modal={modal} />
+  </div>
+  )
+}
