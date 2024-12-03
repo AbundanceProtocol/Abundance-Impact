b536c1e83e4b724811cac7918c59a212f4c87608
Mike Natanzon <50894218+AbundanceProtocol@users.noreply.github.com>
2024-12-03 12:26:09 -0500

    Minor frame update

--- models/Circle.js
+++ models/Circle.js
@@ -3,15 +3,22 @@ import mongoose from 'mongoose';
 const circleSchema = new mongoose.Schema({
   fid: Number,
   time: String,
-  channels: { type: [String], default: []},
-  curators: { type: [Number], default: []},
+  channels: { type: [String], default: [] },
+  curators: { type: [Number], default: [] },
   points: String,
   text: String,
   username: String,
   ecosystem: String,
   referrer: Number,
-  circles: { type: [String], default: []},
-  createdAt: { type: Date, default: () => new Date() }
+  circles: { type: [String], default: [] },
+  showcase: [
+    {
+      cast: { type: String, default: null },
+      circle: { type: String, default: null },
+      username: { type: String, default: null }
+    },
+  ],
+  createdAt: { type: Date, default: () => new Date() },
 });
 
 const Circle = mongoose.models.Circle || mongoose.model('Circle', circleSchema);
