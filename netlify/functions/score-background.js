fcbff8df60d8fd57d658ae91a5ba8084d5da86fa
Mike Natanzon <michael@abundance.id>
2024-12-25 23:49:35 -0500

    Add Score background function

--- netlify/functions/score-background.js
+++ netlify/functions/score-background.js
@@ -4,7 +4,8 @@ import connectToDatabase from '../../libs/mongodb';
 import Cast from '../../models/Cast';
 import Impact from '../../models/Impact';
 import Quality from '../../models/Quality';
-import Raffle from '../../models/Raffle';
+// import Raffle from '../../models/Raffle';
+import Score from '../../models/Score';
 // import User from '../../models/User';
 import Tip from '../../models/Tip';
 // import EcosystemRules from '../../models/EcosystemRules';
@@ -17,13 +18,15 @@ import Tip from '../../models/Tip';
 exports.handler = async function(event, context) {
 
 
-  async function updateRaffleScores() {
+  async function updateImpactScores() {
 
     try {
       await connectToDatabase()
-      const oneWeek = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
-      const threeDays = new Date(Date.now() - 3 * 24 * 60 * 60 * 1000);
-      const oneDay = new Date(Date.now() - 1 * 24 * 60 * 60 * 1000);
+
+      const oneMonth = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+      // const oneWeek = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+      // const threeDays = new Date(Date.now() - 3 * 24 * 60 * 60 * 1000);
+      // const oneDay = new Date(Date.now() - 1 * 24 * 60 * 60 * 1000);
 
       const uniqueCuratorFids = await Impact.distinct('curator_fid', { createdAt: { $gte: threeDays } });
 
@@ -42,33 +45,30 @@ exports.handler = async function(event, context) {
 
       for (const userFid of mergedFids) {
 
-        const userdata_7d = await getUserData(userFid, oneWeek);
-        const userdata_3d = await getUserData(userFid, threeDays);
-        const userdata_24h = await getUserData(userFid, oneDay);
+        const userdata_all = await getUserData(userFid);
+        const userdata_30d = await getUserData(userFid, oneMonth);
 
         const update = {
           $set: {
             points: '$IMPACT', 
-            degen_tip_7d: userdata_7d[0]?.degen_tip || 0,
-            ham_tip_7d: userdata_7d[0]?.ham_tip || 0,
-            curator_points_7d: userdata_7d[0]?.curator_points || 0,
-            impact_score_7d: userdata_7d[0]?.impact_score || 0,
-            degen_tip_3d: userdata_3d[0]?.degen_tip || 0,
-            ham_tip_3d: userdata_3d[0]?.ham_tip || 0,
-            curator_points_3d: userdata_3d[0]?.curator_points || 0,
-            impact_score_3d: userdata_3d[0]?.impact_score || 0,
-            degen_tip_24h: userdata_24h[0]?.degen_tip || 0,
-            ham_tip_24h: userdata_24h[0]?.ham_tip || 0,
-            curator_points_24h: userdata_24h[0]?.curator_points || 0,
-            impact_score_24h: userdata_24h[0]?.impact_score || 0,
-          }
-        }
+            degen_tip_all: userdata_all[0]?.degen_tip || 0,
+            ham_tip_all: userdata_all[0]?.ham_tip || 0,
+            curator_points_all: userdata_all[0]?.curator_points || 0,
+            creator_points_all: userdata_all[0]?.creator_points || 0,
+            impact_score_all: userdata_all[0]?.impact_score || 0,
+            degen_tip_30d: userdata_30d[0]?.degen_tip || 0,
+            ham_tip_30d: userdata_30d[0]?.ham_tip || 0,
+            curator_points_30d: userdata_30d[0]?.curator_points || 0,
+            creator_points_30d: userdata_30d[0]?.creator_points || 0,
+            impact_score_30d: userdata_30d[0]?.impact_score || 0,
+          },
+        };
 
         try {
-          const updatedDoc = await Raffle.findOneAndUpdate({ fid: userFid }, update, updateOptions);
-          // console.log(`Raffle document for fid ${userFid} updated:`, updatedDoc);
+          const updatedDoc = await Score.findOneAndUpdate({ fid: userFid }, update, updateOptions);
+          // console.log(`Score document for fid ${userFid} updated:`, updatedDoc);
         } catch (error) {
-          console.error(`Error updating Raffle document for fid ${userFid}:`, error);
+          console.error(`Error updating Score document for fid ${userFid}:`, error);
         }
       }
       
@@ -82,7 +82,7 @@ exports.handler = async function(event, context) {
 
   }
 
-  const mergedFids = await updateRaffleScores();
+  const mergedFids = await updateImpactScores();
 
   console.log('mergedFids', mergedFids);
 
@@ -97,20 +97,19 @@ exports.handler = async function(event, context) {
 async function getUserData(fid, time) {
   try {
     await connectToDatabase();
-    // const oneWeek = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
 
     let curatorQuery = {};
-    // let creatorQuery = {};
+    let creatorQuery = {};
     let tipperQuery = {};
     curatorQuery.curator_fid = fid
-    // creatorQuery.author_fid = fid
-    // creatorQuery.points = '$IMPACT'
+    creatorQuery.author_fid = fid
+    creatorQuery.points = '$IMPACT'
     tipperQuery.tipper_fid = fid
     tipperQuery.points = '$IMPACT'
 
     if (time) {
       curatorQuery.createdAt = { $gte: time }
-      // creatorQuery.createdAt = { $gte: time }
+      creatorQuery.createdAt = { $gte: time }
       tipperQuery.createdAt = { $gte: time }
     }
     
@@ -187,22 +186,22 @@ async function getUserData(fid, time) {
 
 
     //// CREATOR POINTS
-    // const uniqueAuthorsImpactTotal = await Cast.aggregate([
-    //   { $match: creatorQuery },
-    //   {
-    //     $group: {
-    //       _id: "$author_fid",
-    //       creator_points: { $sum: "$impact_total" },
-    //     },
-    //   },
-    //   {
-    //     $sort: { creator_points: -1 },
-    //   },
-    // ]);
+    const uniqueAuthorsImpactTotal = await Cast.aggregate([
+      { $match: creatorQuery },
+      {
+        $group: {
+          _id: "$author_fid",
+          creator_points: { $sum: "$impact_total" },
+        },
+      },
+      {
+        $sort: { creator_points: -1 },
+      },
+    ]);
 
-    // const creatorDataset = uniqueAuthorsImpactTotal.map(author => {
-    //   return { fid: author._id, creator_points: author.creator_points };
-    // });
+    const creatorDataset = uniqueAuthorsImpactTotal.map(author => {
+      return { fid: author._id, creator_points: author.creator_points };
+    });
 
 
 
@@ -218,15 +217,15 @@ async function getUserData(fid, time) {
       curatorDataset.map((entry) => [entry.fid, { curator_points: entry.curator_points }])
     );
   
-    // const creatorMap = Object.fromEntries(
-    //   creatorDataset.map((entry) => [entry.fid, { creator_points: entry.creator_points }])
-    // );
+    const creatorMap = Object.fromEntries(
+      creatorDataset.map((entry) => [entry.fid, { creator_points: entry.creator_points }])
+    );
   
     // Get the union of all fids
     const allFids = new Set([
       ...tipperDataset.map((entry) => entry.fid),
       ...curatorDataset.map((entry) => entry.fid),
-      // ...creatorDataset.map((entry) => entry.fid),
+      ...creatorDataset.map((entry) => entry.fid),
     ]);
   
     // Merge datasets
@@ -235,28 +234,27 @@ async function getUserData(fid, time) {
       degen_tip: tipperMap[fid]?.degen_tip || 0,
       ham_tip: tipperMap[fid]?.ham_tip || 0,
       curator_points: curatorMap[fid]?.curator_points || 0,
-      // creator_points: creatorMap[fid]?.creator_points || 0,
+      creator_points: creatorMap[fid]?.creator_points || 0,
     }));
   
     mergedDataset = mergedDataset.filter(entry => 
       entry.degen_tip !== 0 || 
       entry.ham_tip !== 0 || 
-      entry.curator_points !== 0
-      //  || 
-      // entry.creator_points !== 0
+      entry.curator_points !== 0 || 
+      entry.creator_points !== 0
     );
 
 
 
     //// CALCULATE IMPACT SCORE
     mergedDataset.forEach(entry => {
-      entry.impact_score = (10 * entry.curator_points + 5 * entry.degen_tip + entry.ham_tip) / 1000;
+      entry.impact_score = (10 * entry.curator_points + 5 * entry.degen_tip + entry.ham_tip + 50 * entry.creator_points) / 1000;
     });
 
     mergedDataset.sort((a, b) => b.impact_score - a.impact_score);
 
     
-    // console.log('combinedDataset', mergedDataset)
+    // console.log('combinedDataset', curatorDataset[0], tipperDataset[0], creatorDataset[0], mergedDataset[0])
 
 
     return mergedDataset;
