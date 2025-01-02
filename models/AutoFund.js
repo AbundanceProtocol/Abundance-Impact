f27a53b6e39273d591e4ed0ee7931594fc187baf
Mike Natanzon <michael@abundance.id>
2025-01-01 22:01:41 -0500

    Enhance AutoFund and Fund schemas

--- models/AutoFund.js
+++ models/AutoFund.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 const autoFundSchema = new mongoose.Schema({
   fid: { type: Number, index: true },
   degen_percent: { type: Number, default: 25, index: true },
+  degen_percent_active: { type: Boolean, default: false, index: true },
+  degen_amount: { type: Number, default: 100, index: true },
+  degen_amount_active: { type: Boolean, default: false, index: true },
   active: { type: Boolean, default: false, index: true },
   remaining: { type: Boolean, default: false, index: true },
   points: { type: String, default: '$IMPACT', index: true },
--- models/Fund.js
+++ models/Fund.js
@@ -5,8 +5,11 @@ const fundSchema = new mongoose.Schema({
   degen_amount: { type: Number, index: true },
   points: { type: String, default: '$IMPACT', index: true },
   growth_fund: { type: Number, default: 15 },
+  growth_amount: { type: Number, default: 0 },
   development_fund: { type: Number, default: 10 },
+  development_amount: { type: Number, default: 0 },
   creator_fund: { type: Number, default: 75 },
+  creator_amount: { type: Number, default: 0 },
   createdAt: { type: Date, default: () => new Date(), index: true }
 });
 
