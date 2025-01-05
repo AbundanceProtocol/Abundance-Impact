1395e01d24932c8a23f91bdc0c4fc5f61c8e54f2
Mike Natanzon <michael@abundance.id>
2025-01-04 22:27:29 -0500

    Remove unused getUserAllowance API and enhance MultiTip component with loading state, image handling, and improved share functionality. Added new functions for creating curation circles and updating images, along with error handling improvements.

--- pages/~/ecosystems/[ecosystem]/curators/[username]/multi-tip/index.js
+++ pages/~/ecosystems/[ecosystem]/curators/[username]/multi-tip/index.js
@@ -11,6 +11,8 @@ import Dropdown from '../../../../../../../components/Ecosystem/Dropdown';
 import Modal from '../../../../../../../components/Layout/Modals/Modal';
 import qs from "querystring";
 import { getTimeRange } from '../../../../../../../utils/utils';
+import Spinner from '../../../../../../../components/Common/Spinner';
+
 const baseURL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL_PROD : process.env.NEXT_PUBLIC_BASE_URL_DEV;
 
 export default function MultiTip() {
@@ -21,20 +23,16 @@ export default function MultiTip() {
   const [screenHeight, setScreenHeight] = useState(undefined)
   const [textMax, setTextMax] = useState('562px')
   const [feedMax, setFeedMax ] = useState('620px')
-
   const router = useRouter()
   const { ecosystem, username } = router.query;
-
   const [modal, setModal] = useState({on: false, success: false, text: ''})
-
   const initialSubmit = {pass: false, target: null}
   const [submitCheck, setSubmitCheck] = useState(initialSubmit)
-
   const [frameTime, setFrameTime] = useState({frame: '7 Days', url: '7d', isSet: 'working', condition: '7d'})
   const [sched, setSched] = useState({curators: false, channels: false, docs: false})
   const [frameCurators, setFrameCurators] = useState({frame: [9326], url: [9326], isSet: 'working', condition: 'abundance'})
   const [frameEcosystem, setFrameEcosystem] = useState({frame: 'abundance', url: 'abundance', eco: 'IMPACT', isSet: 'working', condition: 'abundance'})
-
+  const [loading, setLoading] = useState(false)
   const initEcosystems = [
     { value: 'abundance', label: 'Abundance ecosystem' , eco: 'IMPACT'},
   ]
@@ -46,13 +44,14 @@ export default function MultiTip() {
   const [channelOptions, setChannelOptions] = useState(initChannels)
   const [selectedChannel, setSelectedChannel] = useState(' ')
   const [docNum, setDocNum] = useState(0)
-
+  const [curatorData, setCuratorData] = useState(null)
+  const [imgSrc, setImgSrc] = useState(null)
+  const [showcase, setShowcase] = useState([])
   const initCurators = [
     { value: 'none', label: 'Choose curator', fid: null },
     { value: 'abundance', label: '@abundance', fid: 9326 },
   ]
   const [curatorOptions, setCuratorOptions] = useState(initCurators)
-  
   const timeframe = [
     { value: 'none', label: 'Choose timeframe' },
     { value: '24h', label: '24 Hours' },
@@ -101,18 +100,122 @@ export default function MultiTip() {
   }, [frameCurators, frameEcosystem, frameTime, selectedChannel, sched.docs])
 
 
+  async function createCircle(
+    fid,
+    time,
+    curator,
+    channel,
+    showcase
+  ) {
+
+    try {
+      const response = await axios.post(
+        "/api/curation/postCurationCircle",
+        {
+          fid,
+          time,
+          curator,
+          channel,
+          showcase,
+        }
+      );
+      if (response?.data) {
+        return response?.data?.circleId;
+      } else{
+        return null
+      }
+    } catch (error) {
+      console.error("Error creating post:", error);
+      return null
+    }
+  }
+
+
+  async function updateImage(curator, channels, timeframe, showcase, docs) {
+    setSubmitCheck({ pass: false, target: false });
+    let pfp = null;
+    let username = null;
+    if (curator) {
+      pfp = curator.pfp;
+      username = curator.username;
+    }
+    console.log("curator", curator, pfp, username);
+    try {
+      const response = await axios.post(
+        "/api/multi-tip/postFrame",
+        {
+          curator: [{ pfp, username }],
+          channels: [channels] || [],
+          showcase,
+          timeframe,
+        },
+        {
+          responseType: "blob", // Important: tell axios to handle the response as a blob
+        }
+      );
+      console.log(response?.data);
+      const url = URL.createObjectURL(new Blob([response?.data]));
+      setImgSrc(url);
+      setLoading(false);
+      // setImgSrc(response?.data || null);
+      // return response?.data;
+    } catch (error) {
+      console.error("Error creating post:", error);
+      setImgSrc(null);
+      setLoading(false);
+    }
+    if (
+      frameCurators.isSet == "working" &&
+      frameEcosystem.isSet == "working" &&
+      frameTime.isSet == "working" &&
+      docs > 0
+    ) {
+      setSubmitCheck({ pass: true, target: true });
+    } else {
+      setSubmitCheck({ pass: false, target: false });
+    }
+  }
+
+
+
   async function getDocs(points, curators, channels, time) {
+    if (!loading) {
+      setLoading(true);
+    }
     const getTime = getTimeRange(time)
     try {
       const docData = await axios.get('/api/curation/getDocNum', { params: { points, curators, channels, time: getTime } })
       if (docData) {
+        console.log("casts", docData?.data?.curator);
         setDocNum(docData?.data?.docs)
+        setCuratorData(docData?.data?.curator);
+        let showcaseData = []
+        if (docData?.data?.casts?.length > 0) {
+          showcaseData = docData?.data?.casts
+          if (showcaseData?.length > 0 && showcaseData[0]?.impact) {
+            showcaseData.sort((a, b) => b.impact - a.impact);
+          }
+        }
+        setShowcase(showcaseData);
+        updateImage(
+          docData?.data?.curator,
+          channels,
+          time,
+          showcaseData,
+          docData?.data?.docs
+        );
       } else {
         setDocNum(0)
+        setShowcase([])
+        setCuratorData(null)
+        updateImage(frameCurators, channels, time, [], 0);
       }
     } catch (error) {
       console.error('Error updating channels:', error);
       setDocNum(0)
+      setShowcase([]);
+      setCuratorData(null);
+      updateImage(frameCurators, channels, time, [], 0);
     }
   }
 
@@ -200,7 +303,6 @@ export default function MultiTip() {
       }
     } catch (error) {
       console.error('Error creating post:', error);
-      setCuratorOptions(updatedCurators);
     }
   }
 
@@ -222,25 +324,53 @@ export default function MultiTip() {
   }, [])
 
 
-  function setupEcosystem(target) {
+  async function setupEcosystem(target) {
 
     console.log(frameEcosystem?.url, frameTime?.url, fid, frameEcosystem?.eco)
-    let shareUrl = `https://impact.abundance.id/~/ecosystems/${frameEcosystem?.url}/tip-v5?time=${frameTime?.url}&shuffle=true${frameCurators?.url ? '&curators=' + frameCurators?.url[0] : ''}&eco=${frameEcosystem?.eco}${ (selectedChannel !== ' ') ? '&channels=' + selectedChannel : ''}`
+
+    const getCircleId = await createCircle(
+      fid,
+      frameTime.url,
+      curatorData,
+      selectedChannel,
+      showcase
+    );
+
+
+    // let shareUrl = `https://impact.abundance.id/~/ecosystems/${frameEcosystem?.url}/tip-v5?time=${frameTime?.url}&shuffle=true${frameCurators?.url ? '&curators=' + frameCurators?.url[0] : ''}&eco=${frameEcosystem?.eco}${ (selectedChannel !== ' ') ? '&channels=' + selectedChannel : ''}`
+
+    let shareUrl = `https://impact.abundance.id/~/ecosystems/${frameEcosystem?.url}/curation-v1?id=${getCircleId}`
+
+    let tippedCreators = "";
+    if (showcase?.length > 0) {
+      tippedCreators = showcase.reduce((str, creator, index, arr) => {
+        if (!str.includes(creator.username)) {
+          if (str === "") {
+            return "@" + creator.username;
+          }
+          if (index === arr.length - 1 && index !== 0) {
+            return str + " & @" + creator.username + " ";
+          }
+          return str + ", @" + creator.username;
+        }
+        return str;
+      }, "");
+    }
 
     // console.log('shareUrl', shareUrl)
     let shareText = ''
 
     if (frameCurators?.url && frameCurators?.url[0] == fid) {
-      shareText = `I'm supporting great builders & creators on /impact by @abundance.\n\nExplore my curation and support the nominees here:`
+      shareText = `I'm supporting great builders & creators on /impact by @abundance.\n\nMy latest picks feature ${tippedCreators}\n\nExplore my curation and support the nominees here:`;
     } else if (frameCurators?.url?.length > 0) {
 
       if (frameCurators?.url[0] !== fid) {
-        shareText = `Loving @${frameCurators?.condition}'s curation of builders & creators on /impact by @abundance.\n\nExplore @${frameCurators?.condition}'s curation and support their nominees here:`
+        shareText = `Loving @${frameCurators?.condition}'s curation of builders & creators on /impact by @abundance.\n\n@${frameCurators?.condition}'s latest picks feature ${tippedCreators}\n\nExplore @${frameCurators?.condition}'s curation and support the nominees here:`;
       } else {
-        shareText = `I'm supporting great builders & creators in the ${frameEcosystem?.url} ecosystem on /impact by @abundance. Explore the ecosystem and support builders & creators here:`
+        shareText = `I'm supporting great builders & creators on /impact by @abundance.\n\nLatest picks feature ${tippedCreators}\n\nExplore the ecosystem and support builders & creators here:`;
       }
     } else {
-      shareText = `I'm supporting great builders & creators in the ${frameEcosystem?.url} ecosystem on /impact by @abundance. Explore the ecosystem and support builders & creators here:`
+      shareText = `I'm supporting great builders & creators on /impact by @abundance.\n\nLatest picks feature ${tippedCreators}\n\nExplore the ecosystem and support builders & creators here:`;
     }
 
     let encodedShareText = encodeURIComponent(shareText)
@@ -262,14 +392,14 @@ export default function MultiTip() {
     }
   }
 
-  useEffect(() => {
-    console.log('frame', frameCurators.isSet, frameEcosystem.isSet, frameTime.isSet)
-    if (frameCurators.isSet == 'working' && frameEcosystem.isSet == 'working' && frameTime.isSet == 'working' && docNum > 0) {
-      setSubmitCheck({pass: true, target: true})
-    } else {
-      setSubmitCheck({pass: false, target: false})
-    }
-  }, [frameCurators, frameEcosystem, frameTime, docNum])
+  // useEffect(() => {
+  //   console.log('frame', frameCurators.isSet, frameEcosystem.isSet, frameTime.isSet)
+  //   if (frameCurators.isSet == 'working' && frameEcosystem.isSet == 'working' && frameTime.isSet == 'working' && docNum > 0) {
+  //     setSubmitCheck({pass: true, target: true})
+  //   } else {
+  //     setSubmitCheck({pass: false, target: false})
+  //   }
+  // }, [frameCurators, frameEcosystem, frameTime, docNum])
 
 
 
@@ -330,14 +460,94 @@ export default function MultiTip() {
         <div className='filter-item-on' style={{fontWeight: '600', fontSize: isMobile ? '9px' : '10px'}}>multi-tip</div>
       </div>
     </div>
-    <div className='flex-row' style={{margin: '33px 3px 8px 3px', gap: '1rem', justifyContent: 'center'}}>
-    <img src={`${baseURL}/api/multi-tip/frame?${qs.stringify({ status: 'off', curators: frameCurators?.url, points: '$IMPACT', needLogin: false, time: frameTime?.url})}`} style={{width: '100%', height: 'auto', maxWidth: '400px', minWidth: 'auto', minHeight: 'auto', borderRadius: '15px', aspectRatio: '1 / 1', border: '3px solid #abc'}} /></div>
 
-    <div className='flex-row' style={{margin: '33px 3px 8px 3px', gap: '1rem'}}>
+    <div
+        className="flex-row"
+        style={{
+          margin: "33px 3px 8px 3px",
+          gap: "1rem",
+          justifyContent: "center",
+        }}
+      >
+        {!imgSrc ? (
+          <div style={{ position: "relative" }}>
+            <img
+              src={`${baseURL}/images/backgroundframe3.png`}
+              style={{
+                width: "100%",
+                height: "auto",
+                maxWidth: "400px",
+                minWidth: "auto",
+                minHeight: "auto",
+                borderRadius: "15px",
+                aspectRatio: "1 / 1",
+                border: "3px solid #abc",
+              }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            >
+              <Spinner size={46} color={"#eee"} />
+            </div>
+          </div>
+        ) : imgSrc && !loading ? (
+          <img
+            src={imgSrc}
+            style={{
+              width: "100%",
+              height: "auto",
+              maxWidth: "400px",
+              minWidth: "auto",
+              minHeight: "auto",
+              borderRadius: "15px",
+              aspectRatio: "1 / 1",
+              border: "3px solid #abc",
+            }}
+          />
+        ) : (
+          <div style={{ position: "relative" }}>
+            <img
+              src={imgSrc}
+              style={{
+                width: "100%",
+                height: "auto",
+                maxWidth: "400px",
+                minWidth: "auto",
+                minHeight: "auto",
+                borderRadius: "15px",
+                aspectRatio: "1 / 1",
+                border: "3px solid #abc",
+              }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            >
+              <Spinner size={46} color={"#eee"} />
+            </div>
+          </div>
+        )}
+      </div>
+
+
+
+    {/* <div className='flex-row' style={{margin: '33px 3px 8px 3px', gap: '1rem', justifyContent: 'center'}}>
+    <img src={`${baseURL}/api/multi-tip/frame?${qs.stringify({ status: 'off', curators: frameCurators?.url, points: '$IMPACT', needLogin: false, time: frameTime?.url})}`} style={{width: '100%', height: 'auto', maxWidth: '400px', minWidth: 'auto', minHeight: 'auto', borderRadius: '15px', aspectRatio: '1 / 1', border: '3px solid #abc'}} /></div> */}
+
+    {/* <div className='flex-row' style={{margin: '33px 3px 8px 3px', gap: '1rem'}}>
       <Description {...{show: true, text: 'Ecosystem:', padding: '0 0 0 10px' }} />
     </div>
 
-    <Dropdown {...{key: 0, name: 0, value: frameTime.condition, setupEcosystem, target: 0, conditions: ecosystemOptions, cancel: false, isSet: frameEcosystem.isSet, setCondition: setEcosystem, state: true}} />
+    <Dropdown {...{key: 0, name: 0, value: frameTime.condition, setupEcosystem, target: 0, conditions: ecosystemOptions, cancel: false, isSet: frameEcosystem.isSet, setCondition: setEcosystem, state: true}} /> */}
 
     <div className='flex-row' style={{margin: '33px 3px 8px 3px', gap: '1rem'}}>
       <Description {...{show: true, text: 'Curators:', padding: '0 0 0 10px' }} />
@@ -366,7 +576,18 @@ export default function MultiTip() {
 
 
     <div className='flex-row' style={{margin: '33px 3px 8px 3px', gap: '1rem', justifyContent: 'center'}}>
-      <Button {...{text: 'Create Frame', size: 'large', setupEcosystem, target: submitCheck.target, isSelected: false, submit: submitCheck.target}} />
+    <Button
+      {...{
+        text: "Create Frame",
+        size: "large",
+        setupEcosystem,
+        target: submitCheck.target,
+        isSelected: false,
+        submit: submitCheck.target,
+        loading,
+        textLoading: "Generating frame...",
+      }}
+    />
       <div></div>
     </div>
 
