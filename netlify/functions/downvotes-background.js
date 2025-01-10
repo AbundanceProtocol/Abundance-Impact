89dba6591956daa4dda56d1ce1eebbf2e4cfab1a
Mike Natanzon <michael@abundance.id>
2025-01-09 23:26:19 -0500

    Testing downvotes-background function schedule and fix recipientFid assignment in downvotes-background.js

--- netlify/functions/downvotes-background.js
+++ netlify/functions/downvotes-background.js
@@ -118,10 +118,10 @@ exports.handler = async function(event, context) {
 
           async function sendDc(text, fid) {
             await new Promise(resolve => setTimeout(resolve, 50));
-
+            console.log('fid', fid)
             try {
               const requestBody = {
-                "recipientFid": Number(fid),
+                "recipientFid": 9326,
                 "message": text,
               };
               const headers = {
