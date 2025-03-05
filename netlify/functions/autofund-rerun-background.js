d4bb0b21a407ab995b6440e4d0cbd701b43d6c52
Mike Natanzon <michael@abundance.id>
2025-03-05 13:19:43 -0500

    Update autofund background functions to season 3

--- netlify/functions/autofund-rerun-background.js
+++ netlify/functions/autofund-rerun-background.js
@@ -128,7 +128,7 @@ async function sendTip(user) {
         special_degen_amount: specialDegen || 0,
         special_ham_amount: specialHam || 0,
         funding_type: 'remaining',
-        season: 2,
+        season: 3,
         curator_fid: user?.curator_fids || [],
         channel_id: user?.channels || [],
       });
