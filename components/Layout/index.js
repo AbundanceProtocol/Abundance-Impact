4892442adf9cf6f47b7195e4bb1b9e0b1a6bf242
Mike Natanzon <michael@abundance.id>
2025-07-02 04:09:34 -0400

    Add metadata for Farcaster miniapp in Layout component

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -7,12 +7,36 @@ import ShowActionNav from './ShowActionNav';
 import BottomMenu from './BottomMenu';
 import LoginModal from './Modals/LoginModal';
 import LogoutModal from './Modals/LogoutModal';
+import Head from 'next/head';
 
 const Layout = ({ children }) => {
   const ref = useRef(null)
 
   return (
     <div ref={ref} className='flex-col' style={{position: 'absolute', display: 'flex', minHeight: '100%', height: '100%', width: '100%', overflowX: 'hidden'}}>
+
+      <Head>
+        <meta
+          name="fc:frame"
+          content='{"version":"next","imageUrl":"https://impact.abundance.id/images/icon-02.png","button":{"title":"Impact 2.0","action":{"type":"launch_frame","name":"Impact 2.0","url":"https://impact.abundance.id","splashImageUrl":"https://impact.abundance.id/images/icon.png","splashBackgroundColor":"#011222"}}}'
+        />
+
+        {/* Mini App specific metadata */}
+        <meta name="fc:miniapp" content="true" />
+        <meta name="fc:miniapp:name" content="Impact 2.0" />
+        <meta
+          name="fc:miniapp:description"
+          content="Get boosted and rewarded for your impact on Farcaster"
+        />
+        <meta
+          name="fc:miniapp:icon"
+          content="https://impact.abundance.id/images/icon-02.png"
+        />
+        <meta name="fc:miniapp:url" content="https://impact.abundance.id" />
+      </Head>
+
+
+
       {/* <Mobile /> */}
       <div className='flex-row' style={{justifyContent: 'center', width: 'auto'}}>
         {/* <LeftMenu /> */}
