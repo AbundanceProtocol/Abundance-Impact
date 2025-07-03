46c79f9335dc56ffbcc1fd1fe8d3fb649cc84e5f
Mike Natanzon <michael@abundance.id>
2025-07-03 14:57:22 -0400

    Add cast.js component for miniapp functionality and update package.json dependencies

---
+++ pages/~/miniapp/cast.js
@@ -0,0 +1,236 @@
+import { useRouter } from 'next/router';
+import { useRef, useContext, useEffect, useState } from 'react';
+import Link from 'next/link'
+import axios from 'axios';
+// import { AiOutlineBars } from "react-icons/ai";
+import { useInView } from 'react-intersection-observer'
+// import { BsClock } from "react-icons/bs";
+// import { BiSortDown, BiSortUp } from "react-icons/bi";
+// import { IoShuffleOutline as ShuffleIcon } from "react-icons/io5";
+// import { PiClockClockwiseBold as ClockForward, PiClockCounterClockwiseBold as ClockBack } from "react-icons/pi";
+import { AccountContext } from '../../../context';
+// import { confirmUser } from '../../../utils/utils';
+import Spinner from '../../../components/Common/Spinner';
+import ExpandImg from '../../../components/Cast/ExpandImg';
+// import CuratorData from '../../../components/Page/CuratorData';
+// import { formatNum, getCurrentDateUTC, getTimeRange, isYesterday, checkEmbedType, populateCast, isCast } from '../../../utils/utils';
+import Cast from '../../../components/Cast'
+import useMatchBreakpoints from '../../../hooks/useMatchBreakpoints';
+
+export default function ProfilePage() {
+  const router = useRouter();
+  const [ref, inView] = useInView()
+  const { castHash, castFid, viewerFid } = router.query
+  // const [user, setUser] = useState(null)
+  const { LoginPopup, isLogged, setPoints, setIsLogged, setFid, miniApp, setMiniApp } = useContext(AccountContext)
+  const ref1 = useRef(null)
+  const [textMax, setTextMax] = useState('430px')
+  const [screenWidth, setScreenWidth ] = useState(undefined)
+  const [screenHeight, setScreenHeight] = useState(undefined)
+  const [feedMax, setFeedMax ] = useState('620px')
+  // const userButtons = ['Curation', 'Casts', 'Casts + Replies']
+  // const [searchSelect, setSearchSelect ] = useState('Curation')
+  const { isMobile } = useMatchBreakpoints();
+  const [userFeed, setUserFeed] = useState(null)
+  // const [prevSearch, setPrevSearch] = useState({author_username: null, getTime: null, channel: null, username: null, text: null, shuffle: null, ecosystem: null, page: 0, order: -1, timeSort: null})
+  const [showPopup, setShowPopup] = useState({open: false, url: null})
+  const initialEco = {
+    channels: [],
+    condition_channels: false,
+    condition_curators_threshold: 1,
+    condition_following_channel: false,
+    condition_following_owner: false,
+    condition_holding_erc20: false,
+    condition_holding_nft: false,
+    condition_points_threshold: 1,
+    condition_powerbadge: false,
+    createdAt: "2024-06-17T03:19:16.065Z",
+    downvote_value: 1,
+    ecosystem_moderators: [],
+    ecosystem_name: 'none',
+    ecosystem_handle: 'abundance',
+    ecosystem_points_name: '$IMPACT',
+    ecosystem_rules: [`Can't do evil`],
+    erc20s: [],
+    fid: 3,
+    nfts: [],
+    owner_name: 'none',
+    percent_tipped: 10,
+    points_per_tip: 1,
+    upvote_value: 1,
+  }
+  const [eco, setEco] = useState(initialEco)
+  const [isSelected, setIsSelected] = useState('none')
+  const [userSearch, setUserSearch] = useState({ search: '' })
+  const [selectedChannels, setSelectedChannels] = useState([])
+  const [channels, setChannels] = useState([])
+  const initialQuery = {author_username: null, shuffle: false, time: '3d', tags: [], channels: [], username: null, order: -1}
+  const [userQuery, setUserQuery] = useState(initialQuery)
+  const queryOptions = {
+    tags: [
+      {
+        text: 'All tags',
+        value: []
+      },
+      {
+        text: 'Art',
+        value: 'art'
+      },
+      {
+        text: 'Dev',
+        value: 'dev'
+      },        
+      {
+        text: 'Content',
+        value: 'content'
+      },
+      {
+        text: 'Vibes',
+        value: 'vibes'
+      },
+    ],
+    time: [
+      {
+        text: '24 hours',
+        value: '24hr'
+      },
+      {
+        text: '3 days',
+        value: '3days'
+      },
+      {
+        text: '7 days',
+        value: '7days'
+      },        
+      {
+        text: '30 days',
+        value: '30days'
+      },
+      {
+        text: 'All',
+        value: 'all'
+      },
+    ]
+  }
+  // const [page, setPage] = useState(1)
+  // const [sched, setSched] = useState({inView: false, user: false, feed: false})
+  const [delay, setDelay] = useState(true)
+  // const [timeframe, setTimeframe] = useState('3d')
+  // const [sortBy, setSortBy] = useState('down')
+  const [shuffled, setShuffled] = useState(false)
+
+
+
+
+  useEffect(() => {
+    (async () => {
+      const { sdk } = await import('@farcaster/frame-sdk');
+  
+      
+      if (sdk.context.location.type === 'cast_share') {
+        const cast = sdk.context.location.cast;
+        
+        // Access enriched cast data
+        console.log(cast.author.username);
+        console.log(cast.hash);
+        console.log(cast.timestamp);
+        console.log(cast);
+        console.log('castHash', castHash, 'castFid', castFid, 'viewerFid', viewerFid);
+        
+
+      }
+
+      sdk.actions.ready()
+
+
+    })();
+  }, []);
+
+
+
+
+
+
+  useEffect(() => {
+    if (screenWidth) {
+      if (screenWidth > 680) {
+        setTextMax(`430px`)
+        setFeedMax('620px')
+      }
+      else if (screenWidth >= 635 && screenWidth <= 680) {
+        setTextMax(`390px`)
+        setFeedMax('580px')
+      }
+      else {
+        setTextMax(`${screenWidth - 190}px`)
+        setFeedMax(`${screenWidth}px`)
+      }
+    }
+    else {
+      setTextMax(`100%`)
+      setFeedMax(`100%`)
+    }
+  }, [screenWidth])
+  
+  function closeImagePopup() {
+    setShowPopup({open: false, url: null})
+  }
+  function closeImagePopup() {
+    setShowPopup({open: false, url: null})
+  }
+
+  function openImagePopup(embed) {
+    let newPopup = { ...showPopup }
+    newPopup.open = true
+    newPopup.url = embed.url
+    setShowPopup(newPopup)
+  }
+
+
+
+  useEffect(() => {
+
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth)
+      setScreenHeight(window.innerHeight)
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+
+
+  const updateCast = (index, newData) => {
+    const updatedFeed = [...userFeed]
+    updatedFeed[index] = newData
+    console.log(newData)
+    setUserFeed(updatedFeed)
+  }
+
+  return (
+    <div className='flex-col' style={{width: 'auto', position: 'relative'}} ref={ref1}>
+      <div className="" style={{padding: '58px 0 0 0'}}>
+      </div>
+
+      <div style={{margin: '0 0 70px 0'}}>
+        {(!userFeed || userFeed.length == 0) ? (
+        <div className='flex-row' style={{height: '100%', alignItems: 'center', width: '100%', justifyContent: 'center', padding: '20px'}}>
+          <Spinner size={31} color={'#999'} />
+        </div>
+        ) : (userFeed.map((cast, index) => (<Cast {...{cast, key: index, index, updateCast, openImagePopup, ecosystem: eco?.ecosystem_points_name, handle: eco?.ecosystem_handle, self: false, app}} />)))}
+        {!delay && !shuffled && (
+          <div className='flex-row' style={{height: '100%', alignItems: 'center', width: '100%', justifyContent: 'center', padding: '20px'}}>
+            <Spinner size={31} color={'#999'} />
+          </div>
+        )}
+      </div>
+      {!delay && (<div ref={ref}>&nbsp;</div>)}
+      <ExpandImg  {...{show: showPopup.open, closeImagePopup, embed: {showPopup}, screenWidth, screenHeight }} />
+    </div>
+  );
+}
