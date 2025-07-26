243ca2f9264ebe0f34d52873e2cffc5ed543fc96
Mike Natanzon <michael@abundance.id>
2025-07-26 16:05:17 -0400

    Testing webhook

--- pages/api/mini-app/test.js
+++ pages/api/mini-app/test.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
-    console.log("✅ Received at test endpoint:", req.body);
+    console.log("✅ Received at test endpoint:");
+    console.log("req.bod", req.body);
     return res.status(200).json({ success: true });
   }
   return res.status(405).json({ error: "Method Not Allowed" });
