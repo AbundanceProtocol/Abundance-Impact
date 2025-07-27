4dcfa33fb1cafd5e90b6a3634fe682116a875b04
Mike Natanzon <michael@abundance.id>
2025-07-26 23:51:56 -0400

    Update to schema

--- models/Miniapp.js
+++ models/Miniapp.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
 const miniappSchema = new mongoose.Schema({
-  event: { type: String, index: true },
+  fid: { type: Number, index: true },
+  active: { type: Boolean, index: true },
   url: { type: String, index: true },
+  token: { type: String, index: true }, // encrypted
   createdAt: { type: Date, default: () => new Date(), index: true }
 });
 
