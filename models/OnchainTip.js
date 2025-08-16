c7a64c82f98b3a65c4c0e85ac5ebf58c6e9cf690
Mike Natanzon <michael@abundance.id>
2025-08-16 01:43:15 -0400

    Added onchain tip schema

---
+++ models/OnchainTip.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const onchainTipSchema = new mongoose.Schema({
+  tipper_fid: { type: Number, index: true },
+  tipper_pfp: { type: String },
+  tipper_username: { type: String },
+  points: { type: String, default: '$IMPACT', index: true },
+  tip: [{
+    currency: String,
+    amount: Number
+  }],
+  receiver: [{
+    fid: Number,
+    pfp: String,
+    username: String,
+    amount: Number
+  }],
+  transaction_hash: { type: String },
+  createdAt: { type: Date, default: () => new Date() }
+});
+
+const OnchainTip = mongoose.models.OnchainTip || mongoose.model('OnchainTip', onchainTipSchema);
+
+export default OnchainTip;
