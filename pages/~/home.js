54f46073bd7cbdcefbccafc53649a175f9c8ff23
Mike Natanzon <michael@abundance.id>
2025-08-17 18:50:20 -0400

    Update Homepage and Tip components:

--- pages/~/home.js
+++ pages/~/home.js
@@ -652,7 +652,6 @@ export default function Homepage({ test }) {
       )}
 
       <div
-        onClick={shareCuration}
         className="flex-row"
         style={{
           justifyContent: "center",
@@ -663,6 +662,7 @@ export default function Homepage({ test }) {
         }}
       >
         <div
+          onClick={shareCuration}
           className="flex-col"
           style={{
             // width: "100%",
@@ -711,7 +711,8 @@ export default function Homepage({ test }) {
           </div>
         </div>
 
-        <div
+        <Link
+          href={"/~/tip"}
           className="flex-col"
           style={{
             // width: "100%",
@@ -740,7 +741,7 @@ export default function Homepage({ test }) {
                   gap: "0.25rem",
                   height: "50px",
                   width: "110px",
-                  backgroundColor: "#aaa"
+                  // backgroundColor: "#aaa"
                 }}
               >
                 {(!isMobile || isMobile) && <BsCurrencyExchange size={20} style={{ width: "21px" }} />}
@@ -758,7 +759,7 @@ export default function Homepage({ test }) {
               </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
 
       <div
@@ -768,7 +769,7 @@ export default function Homepage({ test }) {
           alignItems: "center",
           padding: "20px 10px 0 10px",
           flexWrap: "wrap",
-          gap: "1rem"
+          gap: "0.8rem"
         }}
       >
         <Link
--- pages/~/tip.js
+++ pages/~/tip.js
@@ -1484,7 +1484,7 @@ export default function Tip() {
       <Head>
         <meta
           name="fc:frame"
-          content={`{"version":"next","imageUrl":"${baseURL}/api/frames/tip/onchain-tip-v1","button":{"title":"Onchain Multi-Tip","action":{"type":"launch_frame","name":"Impact 2.0","url":"https://impact.abundance.id/~/tip","splashImageUrl":"https://impact.abundance.id/images/icon.png","splashBackgroundColor":"#011222"}}}`}
+          content='{"version":"next","imageUrl":"https://impact.abundance.id/images/icon-02.png","button":{"title":"Impact Multi-Tip","action":{"type":"launch_frame","name":"Impact 2.0","url":"https://impact.abundance.id/~/tip","splashImageUrl":"https://impact.abundance.id/images/icon.png","splashBackgroundColor":"#011222"}}}'
         />
 
         {/* Mini App specific metadata */}
@@ -1494,6 +1494,23 @@ export default function Tip() {
         <meta name="fc:miniapp:icon" content="https://impact.abundance.id/images/icon-02.png" />
         <meta name="fc:miniapp:url" content="https://impact.abundance.id/~/tip" />
       </Head>
+
+
+
+
+
+      {/* <Head>
+        <meta
+          name="fc:frame"
+          content={`{"version":"next","imageUrl":"${baseURL}/api/frames/tip/onchain-tip-v1","button":{"title":"Onchain Multi-Tip","action":{"type":"launch_frame","name":"Impact 2.0","url":"https://impact.abundance.id/~/tip","splashImageUrl":"https://impact.abundance.id/images/icon.png","splashBackgroundColor":"#011222"}}}`}
+        />
+
+        <meta name="fc:miniapp" content="true" />
+        <meta name="fc:miniapp:name" content="Impact 2.0" />
+        <meta name="fc:miniapp:description" content="Get boosted and rewarded for your impact on Farcaster" />
+        <meta name="fc:miniapp:icon" content="https://impact.abundance.id/images/icon-02.png" />
+        <meta name="fc:miniapp:url" content="https://impact.abundance.id/~/tip" />
+      </Head> */}
 
       {/* Custom Share Modal */}
       {shareModal.on && (
