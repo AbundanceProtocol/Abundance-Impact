306c569b0a78d1868e1a3e501bc478204c3d45d6
Mike Natanzon <michael@abundance.id>
2025-08-19 02:33:12 -0400

    Revert "Refactor farcasterMiniApp to return a SSR-safe connector factory with updated connect method and additional properties for better compatibility with Wagmi v2."
    This reverts commit a1e49c6042bfb2e378306a1221521d849678d9be.

--- utils/miniapp-wagmi-connector-server-stub.js
+++ utils/miniapp-wagmi-connector-server-stub.js
@@ -1,23 +1,15 @@
-// SSR-safe stub returning a Wagmi v2-compatible connector factory
-// Wagmi expects `connectors` to be an array of functions (factories) that receive
-// `{ emitter, chains, storage, transports }` and return a connector object.
 export function farcasterMiniApp() {
-  return ({ emitter, chains }) => ({
+  return {
     id: 'farcaster-miniapp-stub',
     name: 'Farcaster Mini App (SSR Stub)',
     type: 'app',
-    rdns: undefined,
-    setup: () => {},
-    connect: async () => ({
-      accounts: [],
-      chainId: chains?.[0]?.id ?? 1,
-    }),
+    connect: async () => ({ account: null }),
     disconnect: async () => {},
     getProvider: async () => null,
     onAccountsChanged: () => {},
     onChainChanged: () => {},
     onDisconnect: () => {},
-  });
+  };
 }
 
 export default { farcasterMiniApp };
