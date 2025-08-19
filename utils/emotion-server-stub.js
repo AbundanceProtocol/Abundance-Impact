b52926a36f37bc1050510e2f023834205bb82d26
Mike Natanzon <michael@abundance.id>
2025-08-19 01:22:40 -0400

    Update Next.js configuration to disable Emotion compiler transform and add server-side stubs for Emotion components. This change prevents client React API imports on the server, improving compatibility with SSR.

---
+++ utils/emotion-server-stub.js
@@ -0,0 +1,12 @@
+// Minimal SSR stub for Emotion to avoid importing client React APIs on server
+export const CacheProvider = ({ children }) => children;
+export const ThemeProvider = ({ children }) => children;
+export const Global = () => null;
+export const ThemeContext = {};
+export const jsx = () => null;
+export const Fragment = ({ children }) => children;
+export const keyframes = () => {};
+export const useTheme = () => ({});
+export default {};
+
+
