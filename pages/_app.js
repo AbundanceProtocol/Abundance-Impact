8baaab6ac8a61237588458e436a900e66555796c
Mike Natanzon <michael@abundance.id>
2025-08-20 04:10:57 -0400

    Revert "Refactor _app.js to lazy-load providers for improved performance and update image URL generation in MultiTip page for consistency."
    This reverts commit 2d7987b83c8bb56ddb1c4206d7db2993585cbc2e.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/index.css'
 import React, { useEffect, useState, useRef } from 'react'
+import { AccountProvider } from '../context'
+import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }) {
   const initialAccount = { points: '$IMPACT', qdau: 0, impact: 0 }
@@ -29,10 +31,6 @@ export default function App({ Component, pageProps }) {
   if (Component?.disableProviders) {
     return <Component {...pageProps} />
   }
-
-  // Lazy-load providers after the opt-out check
-  const { AccountProvider } = require('../context')
-  const Layout = require('../components/Layout').default
 
   return (
     <AccountProvider initialAccount={initialAccount} ref1={ref1}>
