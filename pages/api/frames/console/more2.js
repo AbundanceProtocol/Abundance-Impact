7ab75e540fbc40486169abefd44fbbd67fb1d6f1
Mike Natanzon <michael@abundance.id>
2025-08-21 04:04:56 -0400

    Revert "Revert "Revert "Revert "Removed unused dependencies""""
    This reverts commit 7a44dbc4d7891f403e348e435c3d9c5a4bb8e8eb.

--- pages/api/frames/console/more2.js
+++ pages/api/frames/console/more2.js
@@ -1,6 +1,5 @@
 import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
 import qs from "querystring";
-import { init, validateFramesMessage } from "@airstack/frames";
 
 const baseURL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL_PROD : process.env.NEXT_PUBLIC_BASE_URL_DEV;
 const HubURL = process.env.NEYNAR_HUB
@@ -8,10 +7,10 @@ const client = HubURL ? getSSLHubRpcClient(HubURL) : undefined;
 
 
 export default async function handler(req, res) {
-  init(process.env.AIRSTACK_API_KEY ?? '')
-  const body = await req.body;
-  const {isValid, message} = await validateFramesMessage(body)
-  console.log('isValid:', isValid)
+  // init(process.env.AIRSTACK_API_KEY ?? '')
+  // const body = await req.body;
+  // const {isValid, message} = await validateFramesMessage(body)
+  // console.log('isValid:', isValid)
   const { untrustedData } = req.body
   const { iB, qB, qT, author, iA, qA, ec, login, pt, cu, impact, ql, cI, hash, handle, rS, oO } = req.query;
   
