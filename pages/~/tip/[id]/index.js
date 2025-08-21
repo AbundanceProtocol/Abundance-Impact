354c0e35d6e79e974cd17840f7bba792ff6387b3
Mike Natanzon <michael@abundance.id>
2025-08-21 03:35:48 -0400

    Revert "Revert "Add 'use client' directive to multiple components and pages for client-side rendering support""
    This reverts commit 3c26e992414e2695fb51999697fecc20dd48dcbb.

--- pages/~/tip/[id]/index.js
+++ pages/~/tip/[id]/index.js
@@ -1,3 +1,4 @@
+'use client'
 import { useRouter } from "next/router";
 import { useRef, useContext, useEffect, useState } from "react";
 import { useAccount, useWriteContract, usePublicClient, useWaitForTransactionReceipt } from 'wagmi';
