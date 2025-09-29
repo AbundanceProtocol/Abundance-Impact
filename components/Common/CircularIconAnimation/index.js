cd011f8903f7ebfae24c080fc688b4e4b8b2be73
Mike Natanzon <michael@abundance.id>
2025-09-29 00:03:54 -0400

    Update to infographic

--- components/Common/CircularIconAnimation/index.js
+++ components/Common/CircularIconAnimation/index.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { BsPerson } from "react-icons/bs";
+import { BsPerson, BsStarFill, BsShieldFillCheck, BsSuitHeartFill, BsCurrencyExchange } from "react-icons/bs";
 
 // Register GSAP plugin
 gsap.registerPlugin(useGSAP);
@@ -11,25 +11,35 @@ const CircularIconAnimation = () => {
   const containerRef = useRef(null);
   
   useGSAP(() => {
-    const icons = containerRef.current.querySelectorAll('.circle-icon');
+    const iconWrappers = containerRef.current.querySelectorAll('.icon-wrapper');
     const labels = containerRef.current.querySelectorAll('.circle-label');
-    if (icons.length === 0) return;
+    const secondaryIcons = containerRef.current.querySelectorAll('.secondary-icon');
+    if (iconWrappers.length === 0) return;
     
     const radius = 133; // Icon radius for 320px container (121px + 10% = 133px) 
     
-    console.log('GSAP Animation starting with', icons.length, 'icons'); // Debug log
+    console.log('GSAP Animation starting with', iconWrappers.length, 'icon wrappers'); // Debug log
     
     // Position icons and labels in a circle
-    icons.forEach((icon, index) => {
+    iconWrappers.forEach((wrapper, index) => {
       const angle = (index * 2 * Math.PI) / 5;
       const iconX = radius * Math.cos(angle - Math.PI / 2);
       const iconY = radius * Math.sin(angle - Math.PI / 2);
       
-      gsap.set(icon, {
+      gsap.set(wrapper, {
         x: iconX,
         y: iconY,
         transformOrigin: "center center"
       });
+      
+      // Position secondary icon relative to the wrapper
+      if (secondaryIcons[index]) {
+        gsap.set(secondaryIcons[index], {
+          x: iconX + 3, // 3px to the right of icon center (5px - 2px northwest)
+          y: iconY + 3, // 3px below icon center (5px - 2px northwest)
+          transformOrigin: "center center"
+        });
+      }
       
       // Position corresponding label directly under the icon
       if (labels[index]) {
@@ -51,6 +61,7 @@ const CircularIconAnimation = () => {
     const tl = gsap.timeline({ repeat: -1 });
     
     // Animate each icon sequentially - color only
+    const icons = containerRef.current.querySelectorAll('.circle-icon');
     icons.forEach((icon, index) => {
       tl.to(icon, {
         duration: 0.5,
@@ -74,6 +85,13 @@ const CircularIconAnimation = () => {
   }, { scope: containerRef, dependencies: [] });
   
   const labels = ["Curator", "Validator", "Booster", "Supporter", "Caster"];
+  const secondaryIcons = [
+    BsStarFill,           // Curator
+    BsShieldFillCheck,    // Validator  
+    BsSuitHeartFill,      // Booster
+    BsCurrencyExchange,   // Supporter
+    null                  // Caster (no secondary icon specified)
+  ];
   
   return (
     <div 
@@ -91,34 +109,57 @@ const CircularIconAnimation = () => {
         // backgroundColor: "rgba(0, 34, 68, 0.3)"
       }}
     >
-      {[...Array(5)].map((_, index) => (
-        <div
-          key={`icon-wrapper-${index}`}
-          style={{
-            position: "absolute",
-            left: "50%",
-            top: "50%",
-            transform: "translate(-50%, -50%)",
-            // backgroundColor: "rgba(0, 255, 0, 0.2)", // Debug background
-            // border: "1px solid green", // Debug border
-            borderRadius: "50%",
-            width: "40px",
-            height: "40px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <BsPerson
-            className="circle-icon"
-            size={36}
-            style={{
-              color: "white",
-              cursor: "pointer"
-            }}
-          />
-        </div>
-      ))}
+      {[...Array(5)].map((_, index) => {
+        const SecondaryIcon = secondaryIcons[index];
+        return (
+          <React.Fragment key={`icon-group-${index}`}>
+            <div
+              className="icon-wrapper"
+              style={{
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                transform: "translate(-50%, -50%)",
+                // backgroundColor: "rgba(0, 255, 0, 0.2)", // Debug background
+                // border: "1px solid green", // Debug border
+                width: "40px",
+                height: "40px"
+              }}
+            >
+               <BsPerson
+                 className="circle-icon"
+                 size={36}
+                 style={{
+                   position: "absolute",
+                   left: "50%",
+                   top: "50%",
+                   transform: "translate(-50%, -50%)",
+                   color: "white",
+                   cursor: "pointer"
+                 }}
+               />
+            </div>
+            {SecondaryIcon && (
+              <SecondaryIcon
+                className="secondary-icon"
+                size={22}
+                style={{
+                  position: "absolute",
+                  left: "50%",
+                  top: "50%",
+                  transform: "translate(-50%, -50%)",
+                  color: "#ace",
+                  // backgroundColor: "#002244",
+                  borderRadius: "50%",
+                  padding: "3px",
+                  border: "0px solid #ace",
+                  zIndex: 10
+                }}
+              />
+            )}
+          </React.Fragment>
+        );
+      })}
       {labels.map((label, index) => (
         <div
           key={`label-${index}`}
