b882fc038c469a786af553932036cb7b49eb47be
Mike Natanzon <michael@abundance.id>
2025-10-02 02:24:16 -0400

    Testing channel image

--- pages/api/frames/tip/circle-v7.jsx
+++ pages/api/frames/tip/circle-v7.jsx
@@ -5,13 +5,6 @@ import { promisify } from 'util';
 import svg2img from 'svg2img';
 import fetch from 'isomorphic-unfetch';
 import NodeCache from 'node-cache';
-import Tip from "../../../../models/Tip";
-import Cast from "../../../../models/Cast";
-import Circle from "../../../../models/Circle";
-import User from "../../../../models/User";
-import connectToDatabase from "../../../../libs/mongodb";
-import { numToText } from "../../../../utils/utils";
-import mongoose from 'mongoose';
 import sharp from "sharp";
 
 const baseURL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL_PROD : process.env.NEXT_PUBLIC_BASE_URL_DEV;
@@ -24,76 +17,6 @@ export default async function handler(req, res) {
   try {
     const fontPath = path.join(process.cwd(), 'public', 'Inter-SemiBold.ttf');
     const fontData = fs.readFileSync(fontPath);
-
-    async function getCircle(fid) {
-      try {
-        // console.log(id)
-        await connectToDatabase();
-        let circle = null
-        if (fid) {
-          circle = await Circle.findOne({ fid }).sort({ createdAt: -1 }).exec();
-        // } else if (id) {
-        //   const objectId = new mongoose.Types.ObjectId(id)
-        //   circle = await Circle.findOne({ _id: objectId }).exec();
-        }
-        if (circle && !circle?.image) {
-          return {circles: circle.circles, text: circle.text, showcase: circle.showcase, userPfp: circle.user_pfp || null, curator: circle.curator || [], timeframe: circle.time || '', channels: circle.channels || [], image: null}
-        } else if (circle && circle?.image) {
-          return {circles: circle.circles, text: circle.text, showcase: circle.showcase, userPfp: circle.user_pfp || null, curator: circle.curator || [], timeframe: circle.time || '', image: null, channels: circle.channels || []}
-        } else {
-          return {circles: [], text: '', showcase: [], userPfp: null, curator: [], timeframe: '', image: null}
-        }
-      } catch (error) {
-        console.error("Error while fetching casts:", error);
-        return {circles: [], text: '', showcase: [], userPfp: null, curator: [], timeframe: '', channels: [], image: null}
-      }  
-    }
-
-    async function updateImage(fid, image) {
-      try {
-        // console.log(id)
-        await connectToDatabase();
-        let circle = null
-        
-        if (fid) {
-          circle = await Circle.findOne({ fid }).sort({ createdAt: -1 }).exec();
-        // } else if (id) {
-        //   const objectId = new mongoose.Types.ObjectId(id)
-        //   circle = await Circle.findOne({ _id: objectId }).exec();
-        }
-
-        if (circle) {
-          circle.image = image;
-          await circle.save();
-        }
-        return 
-      } catch (error) {
-        console.error("Error while fetching casts:", error);
-        return
-      }  
-    }
-
-    async function getImageFileSize(url) {
-      try {
-        const response = await fetch(url, { method: 'HEAD' });
-        if (response.ok) {
-          const contentLength = response.headers.get('content-length');
-          if (contentLength) {
-            // typical "Cast preview unavailable" image size
-            if (parseInt(contentLength, 10) < 14500) {
-              return true
-            }
-          }
-          return false
-        } else {
-          console.error('Failed to fetch the image:', response.status, response.statusText);
-          return false;
-        }
-      } catch (error) {
-        console.error('Error fetching the image size:', error);
-        return false;
-      }
-    }
 
     async function getUser(channel) {
       try {
@@ -116,7 +39,7 @@ export default async function handler(req, res) {
 
     const pfp = await getUser(channel)
 
-    console.log('pfp', pfp, 'channel', channel)
+    // console.log('pfp', pfp, 'channel', channel)
 
     let showcase = null
 
@@ -140,14 +63,14 @@ export default async function handler(req, res) {
         }}>
   
     
-          {threshold && (<div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: '0.75rem', border: '0px solid #eeeeeeaa', width: 'auto', margin: '3px 5px 10px 2px', height: '240px'}}>
+          <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: '0.75rem', border: '0px solid #eeeeeeaa', width: 'auto', margin: '3px 5px 10px 2px', height: '240px'}}>
   
 
             <div style={{display: 'flex', textAlign: 'center', color: '#ddeeffee', fontSize: '52px', margin: '0', width: '300px'}}>Curated Channel</div>
 
 
             <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '0.75rem', border: '0px solid #eeeeeeaa', width: 'auto', margin: '3px 5px 10px 2px', height: '240px'}}>
-            {pfp && (<img src={pfp || "https://ipfs.decentralized-content.com/ipfs/bafkreialf5usxssf2eu3e5ct37zzdd553d7lg7oywvdszmrg5p2zpkta7u"} width={100} height={100} style={{borderRadius: '80px', border: '2px solid #eee', backgroundColor: '#8363ca'}} />)}
+            <img src={pfp || `https://ipfs.decentralized-content.com/ipfs/bafkreialf5usxssf2eu3e5ct37zzdd553d7lg7oywvdszmrg5p2zpkta7u`} width={100} height={100} style={{borderRadius: '80px', border: '2px solid #eee', backgroundColor: '#8363ca'}} />
 
             {channel && (<div style={{display: 'flex', flexDirection: 'row', color: 'black', alignItems: 'center', justifyContent: 'center', gap: '0.25rem', border: '1px solid #eeeeeeaa', borderRadius: '16px', padding: '3px 10px', background: '#eeeeeeaa', width: 'auto', margin: '0'}}>
 
@@ -155,39 +78,9 @@ export default async function handler(req, res) {
             </div>)}
 
           </div>
-
   
-          </div>)}
-    
-  
-  
-          {(showcase && showcase?.length >= 1) && (<div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '120px'}}>
-  
-  
-  
-  
-          <div style={{gap: '0.5rem', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', flexDirection: 'row'}}>
-           
-            {showcase?.length >= 1 && (
-              showcase.map((show, index) => (
-                <div key={index} style={{display: 'flex', justifyContent: 'center', alignItems: 'center', minWidth: '65px', maxWidth: '140px', position: 'relative'}}>
-                  <img src={show.cast} width={100} height={100} style={{display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', border: '2px solid #eee', backgroundColor: '#8363ca', minWidth: '65px', maxWidth: '170px'}} />
-                  {show?.impact && (
-                    <div style={{position: 'absolute', bottom: 0, right: 0, transform: 'translate(-0%, -0%)', backgroundColor: '#86c', color: '#fff', fontSize: '15px', padding: '2px 4px', borderRadius: '3px' }}>
-                      {show.impact.toString()}
-                    </div>
-                  )}
-                </div>
-              ))
-            )
-              
-            }
           </div>
-          </div>)}
-  
-  
-
-  
+    
         </div>
         ,
         {
